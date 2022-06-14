7009dec52975f065816d8e74b02ff570c6bab626
Taras Nester <tarasnester414@gmail.com>
2022-06-14 11:54:04 +0300

    added commit request

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from managers import ActorManager
 
 if __name__ == "__main__":
     Actor.objects = ActorManager()
-
     Actor.objects.create(new_first_name="Emma", new_last_name="Watson")
     Actor.objects.create(new_first_name="Daniel", new_last_name="Radclife")
     print(Actor.objects.all())
