db3bb12684f0b6bf863d80c8c29698dfe6e980f0
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-06-16 21:45:26 +0300

    'fix_order_of_parameter'

--- app/managers.py
+++ app/managers.py
@@ -26,7 +26,7 @@ class ActorManager:
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ?"
             "WHERE id = ?",
-            (id_to_update, first_name, last_name)
+            (first_name, last_name, id_to_update)
         )
         self._connection.commit()
 
