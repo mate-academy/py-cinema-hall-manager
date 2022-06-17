c82e167a1a754540c63fd2181de358d3beea9376
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-06-17 12:31:21 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -25,12 +25,12 @@ class ActorManager:
     def update(self, id_: int, new_f_name: str, new_l_name: str):
         self._connection.execute(
             f"UPDATE {self._table_name} SET first_name = ?, last_name = ?"
-            "WHERE id == ?", (new_f_name, new_l_name, id_)
+            "WHERE id = ?", (new_f_name, new_l_name, id_)
         )
         self._connection.commit()
 
     def delete(self, id_: int):
         self._connection.execute(
-            f"DELETE FROM {self._table_name} WHERE id == ?", (id_,)
+            f"DELETE FROM {self._table_name} WHERE id = ?", (id_,)
         )
         self._connection.commit()
