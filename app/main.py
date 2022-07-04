a76316dca96fdd6b273eeb306389deb5e2854605
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-04 20:40:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from models import Actor
 from managers import ActorManager
 
 if __name__ == "__main__":
-     Actor.objects = ActorManager()
+Actor.objects = ActorManager()
 
 Actor.objects.create(first_name="Emma", last_name="Watson")
 Actor.objects.create(first_name="Daniel", last_name="Radclife")
