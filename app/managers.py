bf2369acb5a1da804be9cf184b5fe131007b93c1
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-09-08 12:30:07 +0300

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -9,19 +9,24 @@ class ActorManager:
 
     def create(self, first_name, last_name):
         self.manager.execute(f"INSERT INTO {self._table_name} "
-                             f"(first_name, last_name) VALUES(?, ?)", (first_name, last_name))
+                             f"(first_name, last_name) VALUES(?, ?)",
+                             (first_name, last_name))
         self.manager.commit()
 
     def update(self, id_to_update, new_first_name, new_last_name):
         self.manager.execute(f"UPDATE {self._table_name} "
                              f"SET first_name = ?, last_name = ? "
-                             f"WHERE id = ?", (new_first_name, new_last_name, id_to_update))
+                             f"WHERE id = ?",
+                             (new_first_name, new_last_name, id_to_update))
         self.manager.commit()
 
     def delete(self, id_to_delete):
-        self.manager.execute(f"DELETE FROM {self._table_name} WHERE id = ?", (id_to_delete, ))
+        self.manager.execute(f"DELETE FROM {self._table_name} "
+                             f"WHERE id = ?",
+                             (id_to_delete, ))
         self.manager.commit()
 
     def all(self):
-        manager_cursor = self.manager.execute(f"SELECT * FROM {self._table_name}")
+        manager_cursor = self.manager.execute(f"SELECT * "
+                                              f"FROM {self._table_name}")
         return [models.Actor(*row) for row in manager_cursor]
