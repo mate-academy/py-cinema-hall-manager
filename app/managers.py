b22c832deb94473e3d9b53438db57ebfcdbeb01e
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-08 15:14:44 +0300

    Run flake8

--- app/managers.py
+++ app/managers.py
@@ -14,23 +14,25 @@ class ActorManager:
         )
         return [Actor(*row) for row in cursor_with_actors]
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES (?, ?)",
             (first_name, last_name,),
         )
         self._connection.commit()
 
     def update(self, id_to_update: int,
                first_name: str,
-               last_name: str):
+               last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?",
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
+            f"WHERE id = ?",
             (first_name, last_name, id_to_update,)
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?",
             (id_to_delete,)
