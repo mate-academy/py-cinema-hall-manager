fabf72dda0925565bb6e92b04bef8894963888a7
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-16 16:59:28 +0300

    Actor

---
+++ app/managers.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+from models import Actor
+
+
+class ActorManager:
+
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema_db.db3")
+        self.table_name = "actors"
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name}"
+            " (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name,)
+        )
+
+        self._connection.commit()
+
+    def all(self) -> list:
+        actor_model_cursor = self._connection.execute(
+            f"SELECT id, first_name, last_name FROM {self.table_name}"
+        )
+
+        return [Actor(*row) for row in actor_model_cursor]
+
+    def update(self, id_update: int,
+               first_name_update: str,
+               last_name_update: str) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name}"
+            " SET first_name = ?, last_name = ?"
+            " WHERE id = ?",
+            (id_update, first_name_update, last_name_update,)
+        )
+
+        self._connection.commit()
+
+    def delete(self, id_delete: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name}"
+            " WHERE id = ?",
+            (id_delete,)
+        )
+
+        self._connection.commit()
