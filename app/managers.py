ffc4493a3c2053f97ad009109d488ccb9174c4af
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-17 09:33:21 +0300

    Solution add better name, new line for FROM and WHERE

--- app/managers.py
+++ app/managers.py
@@ -5,40 +5,41 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connections = sqlite3.connect("cinema_db.db3")
+        self._connection = sqlite3.connect("cinema_db.db3")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connections.execute(
+        self._connection.execute(
             f"INSERT INTO {self.table_name} (first_name, last_name) "
             "VALUES (?, ?)",
             (first_name, last_name)
         )
-        self._connections.commit()
+        self._connection.commit()
 
     def retrieve(self) -> list:
-        actors_cursor = self._connections.execute(
-            f"SELECT id, first_name, last_name FROM {self.table_name}"
+        actors_cursor = self._connection.execute(
+            f"SELECT id, first_name, last_name "
+            f"FROM {self.table_name}"
         )
 
         return [Actor(*row) for row in actors_cursor]
 
     def update(self,
-               id: int,
-               first_name: str,
-               last_name: str) -> None:
-        self._connections.execute(
+               id_to_update: int,
+               first_name_to_update: str,
+               last_name_to_update: str) -> None:
+        self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ?"
             "WHERE id = ?",
-            (id, first_name, last_name)
+            (id_to_update, first_name_to_update, last_name_to_update)
         )
-        self._connections.commit()
+        self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
-        self._connections.execute(
+        self._connection.execute(
             f"DELETE FROM {self.table_name} "
             f"WHERE id = ?",
             (id_to_delete,)
         )
-        self._connections.commit()
+        self._connection.commit()
