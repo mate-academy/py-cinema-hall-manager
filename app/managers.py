4e5b43ed17c05f99946d432e4b1ab6321ba7b3f5
LavrykN <nlavryk@kaaiot.io>
2022-10-17 13:40:12 +0300

    Fix flake8

--- app/managers.py
+++ app/managers.py
@@ -1,14 +1,15 @@
 import sqlite3
+from typing import List
 
 from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} "
             "(first_name, last_name) VALUES (?, ?)",
@@ -17,14 +18,19 @@ class ActorManager:
 
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> List[Actor]:
         actor_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
 
         return [Actor(*row) for row in actor_cursor]
 
-    def update(self, id_to_update: int, first_name: str, last_name: str):
+    def update(
+            self,
+            id_to_update: int,
+            first_name: str,
+            last_name: str
+    ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ? "
@@ -34,7 +40,7 @@ class ActorManager:
 
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE actor_id = ?",
             (id_to_delete,)
