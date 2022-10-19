5d0f9787941cd943a851a3bbcc8c200468d1e598
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-10-19 14:08:00 +0200

    Repair update

--- app/managers.py
+++ app/managers.py
@@ -32,7 +32,7 @@ class ActorManager:
             f"UPDATE {self._table_name} "
             "SET (first_name, last_name) = (?, ?) "
             "WHERE id = ?",
-            (id_to_update, first_name, last_name,)
+            (first_name, last_name, id_to_update)
         )
 
         self._connection.commit()
