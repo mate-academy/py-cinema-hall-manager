0efcc33038c119098ff39677c21c46e698746d80
Illia Artnaiev <ilya.artnaev@gmail.com>
2022-10-19 12:18:22 +0300

    rename DB and remove useless brackets

--- app/managers.py
+++ app/managers.py
@@ -6,7 +6,7 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinema")
+        self._connection = sqlite3.connect("cinema.db")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
@@ -25,14 +25,14 @@ class ActorManager:
 
     def update(self, id_update: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name = (?), last_name = (?)"
-            f"WHERE id=(?)",
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ?"
+            f"WHERE id=?",
             (first_name, last_name, id_update)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id=(?)", (id_to_delete,)
+            f"DELETE FROM {self.table_name} WHERE id=?", (id_to_delete,)
         )
         self._connection.commit()
