67036d75f4b2c5f59f74f138bc03c84dfb6a6719
Diana <daria.blessed18@gmail.com>
2022-11-01 15:29:11 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -8,12 +8,12 @@ class ActorManager:
         self._connection = sqlite3.connect("cinema.db")
         self.table_name = "actors"
 
-    def create(self, first_name: str,
-               last_name: str) -> None:
-        self._connection.execute(f"INSERT INTO {self.table_name}"
-                                 f" (first_name, last_name) VALUES (?)",
-                                 (first_name, last_name)
-                                 )
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?,?) ",
+            (first_name, last_name)
+        )
         self._connection.commit()
 
     def all(self) -> list:
