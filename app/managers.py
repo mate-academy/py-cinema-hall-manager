7338e07774e4add065cf97a9a4c9ff53494dcd00
Yulia Bozhieva <bozhieva93@gmail.com>
2022-11-18 09:57:11 -0400

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -8,10 +8,11 @@ class ActorManager:
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute(f"INSERT INTO {self.table_name} "
-                                 f"(first_name, last_name) VALUES (?, ?)",
-                                 (first_name, last_name)
-                                 )
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
         self._connection.commit()
 
     def all(self) -> list:
@@ -25,16 +26,18 @@ class ActorManager:
                new_first_name: str,
                new_last_name: str) -> None:
 
-        self._connection.execute(f"UPDATE {self.table_name} "
-                                 f"SET first_name = ?, last_name = ? "
-                                 f"WHERE id = ?",
-                                 (new_first_name, new_last_name, id_to_update)
-                                 )
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
+            (new_first_name, new_last_name, id_to_update)
+        )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
-        self._connection.execute(f"DELETE FROM {self.table_name} "
-                                 f"WHERE id = ?",
-                                 (id_to_delete,)
-                                 )
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            f"WHERE id = ?",
+            (id_to_delete,)
+        )
         self._connection.commit()
