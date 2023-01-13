de9bfe440ca3f16f1f05dffbda0003aff63a7bd4
soobig666 <darkvader.ukr.net@gmail.com>
2023-01-13 13:41:17 +0200

    connect with managers.py to database SQlite3

--- app/managers.py
+++ app/managers.py
@@ -4,5 +4,14 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
-        self._connection = sqlite3.connect("cin")
+    def __init__(self) ->None:
+        self._connection = sqlite3.connect("cinema")
+
+    def all(self) -> None:
+        actor_data = self._connection.execute("SELECT * FROM actors")
+        print(actor_data)
+
+
+if __name__ == '__main__':
+    manager = ActorManager()
+    manager.all()
