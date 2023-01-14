3244b390d6c8d4eeeaf630d8919ab8a329ac1ec7
Derkach Oleksandr <derkach03553@gmail.com>
2023-01-14 16:35:27 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -9,15 +9,15 @@ class ActorManager:
 
     def all(self) -> None:
         actors_cursor = self._connection.execute(
-            f"select id, first_name, last_name from {self.table_name}"
+            f"SELECT id, first_name, last_name FROM {self.table_name}"
         )
 
         return [Actor(*row) for row in actors_cursor]
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"insert into {self.table_name} "
-            f"(first_name, last_name) values (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUE (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
@@ -25,15 +25,15 @@ class ActorManager:
     def update(self, id_to_update: int,
                first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"update {self.table_name} "
-            f"set first_name = ?, last_name = ? "
-            f"where id = {id_to_update}",
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = {id_to_update}",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"delete from {self.table_name} where id = {id_to_delete}"
+            f"DELETE FROM {self.table_name} WHERE id = {id_to_delete}"
         )
         self._connection.commit()
