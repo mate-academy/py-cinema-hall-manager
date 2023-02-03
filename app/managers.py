f6c6a1a47173616e0852ec6867cf8c7059f1abff
Haishuk Pavlo <xomas@ukr.net>
2023-02-03 14:14:11 +0200

    done

--- app/managers.py
+++ app/managers.py
@@ -4,19 +4,20 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema")
         self.table_name = "actors"
 
-    def all(self):
+    def all(self) -> list:
         actors_cursor = self._connection.execute(
             f"SELECT id, first_name, last_name FROM {self.table_name}"
         )
         return [Actor(*row) for row in actors_cursor]
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
@@ -25,7 +26,7 @@ class ActorManager:
                id_to_update: int,
                new_first_name: str,
                new_last_name: str
-               ):
+               ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ?"
@@ -34,17 +35,10 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} "
             "WHERE id = ?",
             (id_to_delete,)
         )
         self._connection.commit()
-
-
-if __name__ == '__main__':
-    manager = ActorManager()
-    manager.delete(2)
-    print(manager.all())
-
