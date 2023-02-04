cd4aee58679077de74ec6a5b8876ade6fc6147d4
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-02-04 23:50:47 +0000

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,13 +4,14 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connecting = sqlite3.connect("cinema.db3")
         self.table = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connecting.execute(
-            f"INSERT INTO {self.table} first_name VALUE (?) last_name VALUE (?)",
+            f"INSERT INTO {self.table} "
+            "first_name VALUE (?) last_name VALUE (?)",
             (first_name, last_name,)
         )
         self._connecting.commit()
@@ -21,7 +22,12 @@ class ActorManager:
         )
         return [Actor(*row) for row in literary_formats_cursor]
 
-    def update(self, id_to_update: int, update_first_name: str, update_last_name: str):
+    def update(
+            self,
+            id_to_update: int,
+            update_first_name: str,
+            update_last_name: str
+    ) -> None:
         self._connecting.execute(
             f"UPDATE {self.table} "
             f"SET first_name = ? last_name = ? "
@@ -30,7 +36,7 @@ class ActorManager:
         )
         self._connecting.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connecting.execute(
             f"DELETE FROM {self.table} "
             f"WHERE id = ?",
