aaed806303b05f323a7329c5a30eaa909347e8f6
Taras Savchyn <savik1992@gmail.com>
2023-02-26 19:23:20 +0200

    add connection.close()

--- app/managers.py
+++ app/managers.py
@@ -15,11 +15,13 @@ class ActorManager:
             (first_name, last_name),
         )
         self._connection.commit()
+        self._connection.close()
 
     def all(self) -> list:
         cinema_cursor = self._connection.execute(
             f"SELECT id, first_name, last_name from {self.table_name}"
         )
+        self._connection.close()
         return [Actor(*row) for row in cinema_cursor]
 
     def update(self, row_id: int, new_first: str, new_last: str) -> None:
@@ -30,8 +32,10 @@ class ActorManager:
             (new_first, new_last, row_id),
         )
         self._connection.commit()
+        self._connection.close()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE from {self.table_name} " "WHERE id = ?", (id_to_delete,)
         )
+        self._connection.close()
