824344a481404848755ab44b94ba06e9f6b81400
Roman Senyshyn <senjaskorp@gmail.com>
2023-03-06 11:52:45 +0200

    3 commit

--- app/managers.py
+++ app/managers.py
@@ -8,7 +8,7 @@ class ActorManager:
         self._connection = sqlite3.connect("cinema.db3")
         self.table_name = "actors"
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute(
             f"SELECT id, first_name, last_name FROM {self.table_name}"
         )
@@ -17,24 +17,24 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} "
-            f"(first_name,last_name) VALUES (?,?)",
+            f"INSERT INTO {self.table_name}"
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} "
-            f"SET first_name=?, last_name=? "
-            f"WHERE id=?",
+            f"UPDATE {self.table_name}"
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = ?",
             (first_name, last_name, actor_id)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} "
+            f"DELETE FROM {self.table_name}"
             "WHERE id = ?",
             (id_to_delete,)
         )
