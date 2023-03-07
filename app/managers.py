c2a3d81459b23472ad18374de6429447f1717d2c
MikhailLyvak <michael.lyvak@gmail.com>
2023-03-07 14:15:44 +0200

    Flake8 changes

--- app/managers.py
+++ app/managers.py
@@ -11,7 +11,8 @@ class ActorManager:
 
     def create(self, first_name_: str, last_name_: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)", (first_name_, last_name_))
+            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            (first_name_, last_name_))
         self._connection.commit()
 
     def all(self) -> list[Actor]:
@@ -26,8 +27,10 @@ class ActorManager:
         new_first_name: str,
         new_last_name: str
     ) -> None:
-        self._connection.execute(f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?;", (
-            new_first_name, new_last_name, id_to_update))
+        self._connection.execute(
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?;",
+            (new_first_name, new_last_name, id_to_update)
+        )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
