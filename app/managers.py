d40dea67e044515fb5ce690897234237033cce26
MikhailLyvak <michael.lyvak@gmail.com>
2023-03-07 14:21:18 +0200

    Changes2

--- app/managers.py
+++ app/managers.py
@@ -11,7 +11,8 @@ class ActorManager:
 
     def create(self, first_name_: str, last_name_: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name}"
+            " (first_name, last_name) VALUES (?, ?)",
             (first_name_, last_name_))
         self._connection.commit()
 
@@ -28,7 +29,8 @@ class ActorManager:
         new_last_name: str
     ) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?;",
+            f"UPDATE {self.table_name}"
+            " SET first_name = ?, last_name = ? WHERE id = ?;",
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
