18aa23a11621437ab7e6a32fe030d1c5579f02e7
David Gluschenko <davidglusch@gmail.com>
2023-03-07 15:57:04 +0200

    1

--- app/managers.py
+++ app/managers.py
@@ -5,7 +5,7 @@ from app.models import Actor
 class ActorManager:
 
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinema.db3")
+        self._connection = sqlite3.connect("../cinema.db3")
         self.table_name = "actors"
 
     def all(self) -> object:
