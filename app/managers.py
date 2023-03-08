44d3b1e540e210acfcc3bf217e672ec495c951c6
nazar-dmytrenko <nazarkadm@mail.com>
2023-03-08 20:07:09 +0200

    fixed annotation Solution

--- app/managers.py
+++ app/managers.py
@@ -16,7 +16,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         cinema_actors_cursor = self._connection.execute(
             "SELECT *"
             f"FROM {self.table_name}"
