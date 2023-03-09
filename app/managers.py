50a461535b5cd974f6dc3e0b3670305ca53f0415
MikhailLyvak <michael.lyvak@gmail.com>
2023-03-09 15:29:07 +0200

    Fixed indentations, added some SQL style

--- app/managers.py
+++ app/managers.py
@@ -6,19 +6,23 @@ from models import Actor
 class ActorManager:
     def __init__(self) -> None:
         self._connection = sqlite3.connect(
-            "cinema.db3")
+            "cinema.db3"
+        )
         self.table_name = "actors"
 
     def create(self, first_name_: str, last_name_: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name}"
             " (first_name, last_name) VALUES (?, ?)",
-            (first_name_, last_name_))
+            (first_name_, last_name_)
+        )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}")
+            "SELECT * "
+            f"FROM {self.table_name}"
+        )
 
         return [Actor(*row) for row in actors_cursor]
 
@@ -30,12 +34,16 @@ class ActorManager:
     ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name}"
-            " SET first_name = ?, last_name = ? WHERE id = ?;",
+            " SET first_name = ?, last_name = ? "
+            "WHERE id = ?;",
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ?;", (id_to_delete, ))
+            f"DELETE FROM {self.table_name} "
+            f"WHERE id = ?;",
+            (id_to_delete, )
+        )
         self._connection.commit()
