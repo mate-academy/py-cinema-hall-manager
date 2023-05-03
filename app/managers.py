edde92db76539c7e30c2bb668e8da94b77cf06db
VladyslavOladko <vladguitar03@gmail.com>
2023-05-03 21:36:37 +0300

    second refactor

--- app/managers.py
+++ app/managers.py
@@ -8,18 +8,19 @@ class ActorManager:
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute(f"INSERT INTO {self.table_name} "
-                                 f"(first_name, last_name) "
-                                 f"VALUES (?, ?)",
-                                 (first_name, last_name)
-                                 )
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) "
+            f"VALUES (?, ?)",
+            (first_name, last_name)
+        )
         self._connection.commit()
 
     def all(self) -> list:
-        actor_cursor = self._connection.execute(f"SELECT id, "
-                                                f"first_name, "
-                                                f"last_name "
-                                                f"FROM {self.table_name}")
+        actor_cursor = self._connection.execute(
+            f"SELECT *"
+            f"FROM {self.table_name}"
+        )
         return [Actor(*line) for line in actor_cursor]
 
     def update(self,
@@ -27,15 +28,17 @@ class ActorManager:
                new_first_name: str,
                new_last_name: str
                ) -> None:
-        self._connection.execute(f"UPDATE {self.table_name} "
-                                 f"SET (first_name, last_name) = (?, ?)"
-                                 f"WHERE id = ?",
-                                 (new_first_name, new_last_name, id_to_update)
-                                 )
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            f"SET (first_name, last_name) = (?, ?)"
+            f"WHERE id = ?",
+            (new_first_name, new_last_name, id_to_update)
+        )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
-        self._connection.execute(f"DELETE FROM {self.table_name} "
-                                 f"WHERE id = ?",
-                                 (id_to_delete,)
-                                 )
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            f"WHERE id = ?",
+            (id_to_delete,)
+        )
