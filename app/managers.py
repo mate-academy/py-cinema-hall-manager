cb0ca417afcfcebb41f83c4c30f11beae521e289
Viktor Rura <ruravictor@gmail.com>
2023-05-22 09:39:48 +0100

    Fixed flake8

--- app/managers.py
+++ app/managers.py
@@ -2,27 +2,32 @@ import sqlite3
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.connection = sqlite3.connect("cinema.db3")
 
-    def all(self):
+    def all(self) -> None:
         self.connection.execute("SELECT * FROM actors")
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         self.connection.execute(
             "INSERT INTO actors (first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self.connection.commit()
 
-    def update(self, id_to_update: int, new_first_name, new_last_name):
+    def update(
+            self,
+            id_to_update: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
         self.connection.execute(
             "UPDATE actors SET first_name=?, last_name=? WHERE id=?",
             (new_first_name, new_last_name, id_to_update)
         )
         self.connection.commit()
 
-    def delete(self, id_to_delete):
+    def delete(self, id_to_delete: int) -> None:
         self.connection.execute(
             "DELETE FROM actors WHERE id = ?",
             (id_to_delete,)
