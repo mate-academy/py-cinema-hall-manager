afbf8c481b3196854a6fda80bf1fa2cb21ac2cf6
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-05-29 13:24:14 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -19,7 +19,7 @@ class ActorManager:
 
     def all(self) -> list:
         actors_cursor = self._connection.execute(
-            f"SELECT id, first_name, last_name"
+            f"SELECT *"
             f" FROM {self.table_name}"
         )
         return [Actor(*row) for row in actors_cursor]
