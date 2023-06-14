300529bba14458a1d250970b1913d616784f0ea1
verdene0 <b.verdeneo@gmail.com>
2023-06-14 22:18:02 +0300

    second fix solution for task py-actor-manager

--- app/managers.py
+++ app/managers.py
@@ -7,7 +7,7 @@ class ActorManager:
         self._connection = sqlite3.connect("cinema.db")
         self.table_name = "actors"
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -16,14 +16,14 @@ class ActorManager:
             Actor(*row) for row in actors_cursor
         ]
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?,?)",
             (first_name, last_name,)
         )
         self._connection.commit()
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name: str):
+    def update(self, id_to_update: int, new_first_name: str, new_last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ? "
@@ -32,7 +32,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} "
             f"WHERE id = ?",
