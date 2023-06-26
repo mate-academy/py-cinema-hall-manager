feafba1bf52fd1977047274f56f0c95a55157c15
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-06-26 22:57:46 +0200

    Fix flake8

--- app/managers.py
+++ app/managers.py
@@ -10,18 +10,28 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name}"
+            f" (first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
-        actor_cursor = self._connection.execute(f"SELECT * FROM {self.table_name}")
+        actor_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
         return [Actor(*row) for row in actor_cursor]
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name: str) -> None:
-        self._connection.execute(f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?",
-                                 (new_first_name, new_last_name, id_to_update))
+    def update(self,
+               id_to_update: int,
+               new_first_name: str,
+               new_last_name: str
+               ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? WHERE id = ?",
+            (new_first_name, new_last_name, id_to_update)
+        )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
