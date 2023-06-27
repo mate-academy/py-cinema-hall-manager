226a321f233821c43ddbb42bdff435dd5f3a318e
YOUR_NAME <YOUR_EMAIL>
2023-06-27 15:24:05 +0300

    Actor manager (first commit)

---
+++ app/managers.py
@@ -0,0 +1,41 @@
+from typing import List
+import sqlite3
+from models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema.db")
+        self.table_name = "actors"
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> List["Actor"]:
+        cinema_formats_cursor = self._connection.execute(
+            f"SELECT id, first_name, last_name FROM {self.table_name};"
+        )
+        return [Actor(*row) for row in cinema_formats_cursor]
+
+    def update(
+            self,
+            id_to_update: int,
+            first_name: str,
+            last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} SET first_name=?, last_name=?"
+            "WHERE id=?", (first_name, last_name, id_to_update)
+        )
+        self._connection.commit()
+
+    def delete(self, id_to_delete: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id=?", (id_to_delete,)
+        )
+        self._connection.commit()
