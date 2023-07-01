d8b8570708416074bf1ab01e3df4e2d4ceca1917
maccarets <maccarets@gmail.com>
2023-07-01 12:46:11 +0300

    fix connection

--- app/managers.py
+++ app/managers.py
@@ -4,31 +4,31 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self.conn = sqlite3.connect("cinema.db3")
+        self.connection = sqlite3.connect("cinema.db3")
         self.table_name = "actors"
 
     def all(self) -> list:
-        actors = self.conn.execute(f"SELECT * FROM {self.table_name}")
+        actors = self.connection.execute(f"SELECT * FROM {self.table_name}")
         return [Actor(*row) for row in actors]
 
     def create(self, first_name: str, last_name: str) -> None:
-        self.conn.execute(f"INSERT INTO {self.table_name} "
+        self.connection.execute(f"INSERT INTO {self.table_name} "
                           f"(first_name, last_name) VALUES (?, ?)",
-                          (first_name, last_name))
-        self.conn.commit()
+                                (first_name, last_name))
+        self.connection.commit()
 
     def update(self, id: int, name: str, surname: str) -> None:
-        self.conn.execute(f"UPDATE {self.table_name} "
+        self.connection.execute(f"UPDATE {self.table_name} "
                           "SET first_name = ?, last_name= ?"
                           "WHERE id = ?",
-                          (name, surname, id))
-        self.conn.commit()
+                                (name, surname, id))
+        self.connection.commit()
 
     def delete(self, id: int) -> None:
-        self.conn.execute(f"DELETE FROM {self.table_name} "
+        self.connection.execute(f"DELETE FROM {self.table_name} "
                           f"WHERE id = ?",
-                          (id,))
-        self.conn.commit()
+                                (id,))
+        self.connection.commit()
 
 
 if __name__ == "__main__":
