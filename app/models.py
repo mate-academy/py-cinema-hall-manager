e8f9056ebc64c4c4fecf62a44797060d1f228652
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-30 16:09:41 +0300

    Solution

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Actor:
+    id: int
+    first_name: str
+    last_name: str
+
+    def __repr__(self) -> str:
+        return (
+            f"Actor(id={self.id}, "
+            f"first_name={self.first_name}, last_name={self.last_name})"
+        )
