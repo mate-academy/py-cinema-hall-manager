e61f0c89e539140624a4c6e84de8bae33241d50f
Max Katkalov <makskatkalov@gmail.com>
2023-09-01 23:55:57 +0300

    inside all method changed to *

--- app/managers.py
+++ app/managers.py
@@ -18,7 +18,7 @@ class ActorManager:
 
     def all(self) -> list[Actor]:
         data_cursor = self._connection.execute(
-            f"SELECT id, first_name, last_name " f"FROM {self.table_name}"
+            f"SELECT * " f"FROM {self.table_name}"
         )
 
         return [Actor(*row) for row in data_cursor]
