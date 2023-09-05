0e30cde9292bbee7367543daba029faaf44adf1f
Kristian Yendrek <kristianendreck4@gmail.com>
2023-09-05 22:55:25 +0300

    implemented sql requests

---
+++ app/managers.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+from app.models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema")
+        self.table_name = "actors"
+
+    def create(self, first_name: str, last_name: str):
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> list:
+        actors_info = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+
+        return [Actor(*row) for row in actors_info]
+
+    def update(
+        self,
+        id_to_upd: str,
+        new_first_name: str,
+        new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ?"
+            "WHERE id = ?",
+            (new_first_name, new_last_name, id_to_upd)
+        )
+        self._connection.commit()
+
+    def delete(self, id_to_del: int):
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (id_to_del,)
+        )
+        self._connection.commit()
