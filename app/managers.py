49af7a93fc4690582639586bb04363dcf41b18be
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-09-06 15:08:38 +0300

    used connection context manager

--- app/managers.py
+++ app/managers.py
@@ -8,50 +8,61 @@ class ActorManager:
         self._connection = sqlite3.connect("../cinema")
         self.table_name = "actors"
 
+        with self._connection:
+            self._connection.execute(
+                f"CREATE TABLE IF NOT EXISTS {self.table_name} "
+                "("
+                "id INTEGER PRIMARY KEY, "
+                "first_name VARCHAR(63) NOT NULL, "
+                "last_name VARCHAR(63) NOT NULL"
+                ")"
+            )
+
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute(
-            f"INSERT INTO {self.table_name} "
-            f"(first_name, last_name) VALUES (?, ?)",
-            (first_name, last_name),
-        )
-        self._connection.commit()
+        with self._connection:
+            self._connection.execute(
+                f"INSERT INTO {self.table_name} "
+                f"(first_name, last_name) VALUES (?, ?)",
+                (first_name, last_name),
+            )
 
     def all(self) -> list:
-        actors_cursor = self._connection.execute("SELECT * FROM actors")
+        with self._connection:
+            actors_cursor = self._connection.execute("SELECT * FROM actors")
 
         return [Actor(*row) for row in actors_cursor]
 
     def update(
         self, id_to_update: int, new_first_name: str, new_last_name: str
     ) -> None:
-        self._connection.execute(
-            f"UPDATE {self.table_name} "
-            f"SET first_name = ?, last_name = ?"
-            f"WHERE id = ?",
-            (new_first_name, new_last_name, id_to_update),
-        )
-        self._connection.commit()
+        with self._connection:
+            self._connection.execute(
+                f"UPDATE {self.table_name} "
+                f"SET first_name = ?, last_name = ?"
+                f"WHERE id = ?",
+                (new_first_name, new_last_name, id_to_update),
+            )
 
     def delete(self, id_to_delete: int) -> None:
-        self._connection.execute(
-            f"DELETE FROM {self.table_name} " f"WHERE id = ?", (id_to_delete,)
-        )
-        self._connection.commit()
+        with self._connection:
+            self._connection.execute(
+                f"DELETE FROM {self.table_name} "
+                f"WHERE id = ?", (id_to_delete,)
+            )
 
     def clear(self) -> None:
-        self._connection.execute(f"DROP TABLE {self.table_name}")
-        self._connection.commit()
+        with self._connection:
+            self._connection.execute(f"DROP TABLE {self.table_name}")
 
-        self._connection.execute(
-            f"CREATE TABLE {self.table_name} ("
-            "    id         INTEGER    PRIMARY KEY AUTOINCREMENT"
-            "                          UNIQUE"
-            "                          NOT NULL,"
-            "    first_name TEXT (255),"
-            "    last_name  TEXT (255) "
-            ")"
-        )
-        self._connection.commit()
+            self._connection.execute(
+                f"CREATE TABLE {self.table_name} ("
+                "    id         INTEGER    PRIMARY KEY AUTOINCREMENT"
+                "                          UNIQUE"
+                "                          NOT NULL,"
+                "    first_name TEXT (255),"
+                "    last_name  TEXT (255) "
+                ")"
+            )
 
 
 if __name__ == "__main__":
