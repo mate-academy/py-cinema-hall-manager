252b05edb5b202dd19909bf3f4d9de71b406feb4
anna-lybid <anna.lybid@gmail.com>
2023-09-06 13:47:32 +0300

    Update with check if there is no table

--- app/managers.py
+++ app/managers.py
@@ -8,6 +8,16 @@ class ActorManager:
         self.table_name = "actors"
         self._connection = sqlite3.connect("cinema.sqlite")
 
+        with self._connection:
+            self._connection.execute(
+                f"CREATE TABLE IF NOT EXISTS {self.table_name} ("
+                "id INTEGER PRIMARY KEY, "
+                "first_name VARCHAR(50) NOT NULL, "
+                "last_name VARCHAR(50) NOT NULL"
+                ")"
+            )
+
+
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} "
