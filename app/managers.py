4496055202ffe4de9eb3514ecfb962f5dfb79c46
Kristian Yendrek <kristianendreck4@gmail.com>
2023-09-07 20:34:03 +0300

    some fixes

--- app/managers.py
+++ app/managers.py
@@ -5,7 +5,7 @@ from app.models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinema")
+        self._connection = sqlite3.connect("cinema.sqlite3")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
@@ -17,18 +17,19 @@ class ActorManager:
         self._connection.commit()
 
     def all(self) -> list:
-        actors_info = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+        actor_cursor = self._connection.execute(
+            "SELECT * "
+            f"FROM {self.table_name}"
         )
 
-        return [Actor(*row) for row in actors_info]
+        return [Actor(*row) for row in actor_cursor]
 
     def update(
         self, id_to_upd: str, new_first_name: str, new_last_name: str
     ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ?"
+            "SET first_name = ?, last_name = ? "
             "WHERE id = ?",
             (new_first_name, new_last_name, id_to_upd),
         )
@@ -36,6 +37,8 @@ class ActorManager:
 
     def delete(self, id_to_del: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ?", (id_to_del,)
+            f"DELETE FROM {self.table_name} "
+            f"WHERE id = ?",
+            (id_to_del, )
         )
         self._connection.commit()
