a755d8d7900acd4a0c192a865cb4277f75b8c279
Eduard Habryd <34722326+eduardhabryd@users.noreply.github.com>
2023-09-07 14:31:19 +0300

    Update models.py, used str instead of repr

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Actor:
     first_name: str
     last_name: str
 
-    def __repr__(self) -> str:
+    def __str__(self) -> str:
         return (
             f"Actor(id={self.id}, "
             f"first_name={self.first_name}, last_name={self.last_name})"
