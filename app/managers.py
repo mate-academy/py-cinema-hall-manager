26d7f97dc54fc0c6f82583addaa0c16ddcc3d6bc
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-09-08 13:06:35 +0300

    indentation fixed

--- app/managers.py
+++ app/managers.py
@@ -9,9 +9,9 @@ class ActorManager:
         self.table_name = "actors"
         self._connection.execute(
             f"""
-        CREATE TABLE IF NOT EXISTS {self.table_name}
-        (id INTEGER PRIMARY KEY,
-        first_name VARCHAR(255), second_name VARCHAR(255))
+            CREATE TABLE IF NOT EXISTS {self.table_name}
+            (id INTEGER PRIMARY KEY,
+            first_name VARCHAR(255), second_name VARCHAR(255))
             """
         )
         self._connection.commit()
@@ -19,9 +19,9 @@ class ActorManager:
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"""
-        INSERT INTO {self.table_name}
-        (first_name, second_name)
-        VALUES (?,?)
+            INSERT INTO {self.table_name}
+            (first_name, second_name)
+            VALUES (?,?)
             """,
             (first_name, last_name),
         )
@@ -30,8 +30,8 @@ class ActorManager:
     def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
             f"""
-        SELECT *
-        FROM {self.table_name}
+            SELECT *
+            FROM {self.table_name}
             """
         )
         return [Actor(*row) for row in actors_cursor]
@@ -43,10 +43,10 @@ class ActorManager:
                ) -> None:
         self._connection.execute(
             f"""
-        UPDATE {self.table_name}
-        SET first_name = ?, second_name = ?
-        WHERE id = ?
-                """,
+            UPDATE {self.table_name}
+            SET first_name = ?, second_name = ?
+            WHERE id = ?
+            """,
             (new_first_name, new_last_name, id_to_update),
         )
         self._connection.commit()
@@ -54,8 +54,8 @@ class ActorManager:
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"""
-        DELETE FROM {self.table_name}
-        WHERE id =?
+            DELETE FROM {self.table_name}
+            WHERE id =?
             """,
             (id_to_delete,)
         )
