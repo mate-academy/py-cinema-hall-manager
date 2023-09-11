685a6f8b94bf9058f740048aa6cf665a2e5ad8b4
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-09-11 12:49:12 +0200

    updated

--- app/managers.py
+++ app/managers.py
@@ -34,13 +34,14 @@ class ActorManager:
         return [Actor(*row) for row in actors_cursor]
 
     def update(
-        self, id_to_update: int, new_first_name: str, new_last_name: str
+            self, id_to_update: int, new_first_name: str, new_last_name: str
     ) -> None:
+
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ?"
             f"WHERE id = ?",
-            (new_first_name, new_last_name, id_to_update),
+            (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
