89c4cb63d45b9854f0b689f6cf6a4a99a0acc6f0
isalata <igorwwoww@gmail.com>
2023-09-13 18:50:43 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -5,17 +5,17 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self) -> str :
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.db")
         self.table_name = "actors"
 
-    def all(self) -> str:
+    def all(self) -> None:
         actor_cursor = self._connection.execute(
             f"SELECT id, first_name, last_name FROM {self.table_name}"
         )
         return [Actor(*row) for row in actor_cursor]
 
-    def create(self, first_name: str, last_name: str) -> str:
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name}"
             f"(first_name, last_name) VALUES (?,?) ",
@@ -24,7 +24,7 @@ class ActorManager:
         self._connection.commit()
 
     def update(self, new_first_name: str, new_last_name: str,
-               id_to_update: int) -> str:
+               id_to_update: int) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ?"
@@ -33,7 +33,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int) -> str:
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} "
             f"WHERE id = ?",
