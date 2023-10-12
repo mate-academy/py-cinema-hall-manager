564e7e0a74f2cea144aa012a8f910c9f2c4f9137
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-10-12 14:03:35 +0300

    fix: db in gitignore

--- app/managers.py
+++ app/managers.py
@@ -4,7 +4,7 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("app/cinema.sqlite3")
+        self._connection = sqlite3.connect("app/cinemadb.sqlite3")
         self._table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
