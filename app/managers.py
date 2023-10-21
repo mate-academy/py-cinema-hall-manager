ab8a79c0bb88b7be810d14acec2e14406c1f46ce
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-10-21 13:22:58 +0300

    solution1

--- app/managers.py
+++ app/managers.py
@@ -4,7 +4,7 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect(
             "C:/Users/andsh/PycharmProjects/MyProjects"
             "/django_orm/tasks_with_git_fork_2/py-actor-manager"
@@ -12,21 +12,27 @@ class ActorManager:
         )
         self.tablename = "actors"
 
-    def all(self):
+    def all(self) -> list:
         actors_data = self._connection.execute(
             f"SELECT * FROM {self.tablename}"
         )
         print("Actors data", actors_data)
         return [Actor(*row) for row in actors_data]
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.tablename} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.tablename} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name: str):
+    def update(
+            self,
+            id_to_update: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
         self._connection.execute(
             f"UPDATE {self.tablename} "
             f"SET first_name = ?, last_name = ? "
@@ -35,24 +41,10 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.tablename} "
             f"WHERE id = ?",
             (id_to_delete,)
         )
         self._connection.commit()
-
-
-if __name__ == '__main__':
-    actor = ActorManager()
-    print(actor.all())
-    print()
-    actor.create("bodya", "messi")
-    print(actor.all())
-    print()
-    actor.update(3, "ya",  "actor")
-    print(actor.all())
-    print()
-    actor.delete(4)
-    print(actor.all())
