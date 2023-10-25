ff558dd4e5bfe393cdd1313e6220c4b38bfc4313
Lylak Ivan <lylakivanmate@gmail.com>
2023-10-25 12:42:23 +0300

    'Solution'

---
+++ main.py
@@ -0,0 +1,19 @@
+from models import Actor
+from managers import ActorManager
+
+if __name__ == "__main__":
+    # 1 Initialization
+
+    Actor.objects = ActorManager()
+
+    # 2 Execute command
+
+    Actor.objects.create(first_name="Bob", last_name="Blak")
+    Actor.objects.create(first_name="Ron", last_name="Red")
+    Actor.objects.create(first_name="Whill", last_name="White")
+    print(Actor.objects.all())
+    Actor.objects.update(13, "Gram", "Green")
+    Actor.objects.update(3, "Peter", "Pink")
+    print(Actor.objects.all())
+    Actor.objects.delete(13)
+    print(Actor.objects.all())
