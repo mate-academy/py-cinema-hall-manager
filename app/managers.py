306c5e07a9be051ddf3707d27977cfd36cae9bb2
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-10-29 13:11:24 +0100

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
 class ActorManager(models.Manager):
-    def create_actor(self, first_name, last_name):
+    def create_actor(self, first_name: str, last_name: str) -> Actor:
         actor = self.create(first_name=first_name, last_name=last_name)
         return actor
 
-    def update_actor(self, actor_id, first_name=None, last_name=None):
+    def update_actor(self, actor_id: int,
+                     first_name: str = None, last_name: str = None) -> None:
         actor = self.get(id=actor_id)
         if first_name is not None:
             actor.first_name = first_name
@@ -14,6 +24,6 @@ class ActorManager(models.Manager):
             actor.last_name = last_name
         actor.save()
 
-    def delete_actor(self, actor_id):
+    def delete_actor(self, actor_id: int) -> None:
         actor = self.get(id=actor_id)
         actor.delete()
