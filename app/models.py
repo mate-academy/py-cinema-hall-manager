3ca1e22f36265eac108c93a241d603cef82f5b07
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-10-29 13:11:39 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,5 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
-
-
