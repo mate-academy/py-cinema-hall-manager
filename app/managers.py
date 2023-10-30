9837f30aa47eb52de57da144728b1fb55152fa63
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-10-30 19:51:17 +0200

    Improved Solution

--- app/managers.py
+++ app/managers.py
@@ -4,7 +4,7 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinema")
+        self._connection = sqlite3.connect("../cinema.db3")
         self.table_name = "actors"
 
     def all(self) -> list[Actor]:
