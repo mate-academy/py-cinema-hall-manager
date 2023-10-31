f3f5a5d1d3aee8cadd94b28a06aa6492b5f3f9f5
Lytillis <chemurchem@gmail.com>
2023-10-31 11:39:41 +0200

    Started using context managers to handle db connection

--- app/managers.py
+++ app/managers.py
@@ -4,20 +4,21 @@ import psycopg2
 
 class ActorManager:
     def __init__(self) -> None:
+        self._db_name = "cinema"
+        self._password = "mate"
+        self._username = "mate"
         self.last_id = 0
-        self.connection = None
 
     def _connect(self) -> None:
-        self.connection = psycopg2.connect(user="mate",
-                                           password="mate",
-                                           host="127.0.0.1",
-                                           port="5432",
-                                           database="cinema")
+        return psycopg2.connect(user=self._username,
+                                password=self._password,
+                                host="127.0.0.1",
+                                port="5432",
+                                database=self._db_name)
 
     def create(self, first_name: str, last_name: str) -> None:
-        try:
-            self._connect()
-            cursor = self.connection.cursor()
+        with self._connect() as conn:
+            cursor = conn.cursor()
 
             query = """
                     INSERT INTO actor(id, first_name, last_name)
@@ -25,18 +26,10 @@ class ActorManager:
                     """
             self.last_id += 1
             cursor.execute(query, (self.last_id, first_name, last_name))
-            self.connection.commit()
-        except (Exception, psycopg2.Error) as error:
-            print(error)
-        finally:
-            if self.connection:
-                cursor.close()
-                self.connection.close()
 
     def all(self) -> list[Actor]:
-        try:
-            self._connect()
-            cursor = self.connection.cursor()
+        with self._connect() as conn:
+            cursor = conn.cursor()
 
             query = """
                     SELECT * FROM actor;
@@ -46,46 +39,25 @@ class ActorManager:
             actors_data = cursor.fetchall()
             actors = [Actor(row[0], row[1], row[2]) for row in actors_data]
             return actors
-        except (Exception, psycopg2.Error) as error:
-            print(error)
-        finally:
-            if self.connection:
-                cursor.close()
-                self.connection.close()
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
-        try:
-            self._connect()
-            cursor = self.connection.cursor()
+        with self._connect() as conn:
+            cursor = conn.cursor()
 
             query = """
                     UPDATE actor SET first_name = %s, last_name = %s
                     WHERE id = %s;
                     """
             cursor.execute(query, (first_name, last_name, id))
-            self.connection.commit()
-        except (Exception, psycopg2.Error) as error:
-            print(error)
-        finally:
-            if self.connection:
-                cursor.close()
-                self.connection.close()
 
     def delete(self, id: int) -> None:
-        try:
-            self._connect()
-            cursor = self.connection.cursor()
+        with self._connect() as conn:
+            cursor = conn.cursor()
 
             query = """
                     DELETE FROM actor
                     WHERE id = %s;
                     """
+
             self.last_id += 1
             cursor.execute(query, (id,))
-            self.connection.commit()
-        except (Exception, psycopg2.Error) as error:
-            print(error)
-        finally:
-            if self.connection:
-                cursor.close()
-                self.connection.close()
