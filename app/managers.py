6ac4aa5c3d0730942e44b5ded240dbd455e19010
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-11-04 16:19:39 +0100

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,29 +1,33 @@
-from django.db import models
+import sqlite3
+from models import Actor
+from typing import List
 
 
-class Actor(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+class ActorManager:
+    def __init__(self, database_name: str = "cinema.db") -> None:
+        self.conn = sqlite3.connect(database_name)
+        self.cursor = self.conn.cursor()
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+    def create(self, actor: Actor) -> None:
+        self.cursor.execute("INSERT INTO actors (first_name, last_name) "
+                            "VALUES (?, ?)",
+                            (actor.first_name, actor.last_name))
+        self.conn.commit()
 
+    def all(self) -> List[Actor]:
+        self.cursor.execute("SELECT * FROM actors")
+        rows = self.cursor.fetchall()
+        actors = [Actor(id=row[0], first_name=row[1],
+                        last_name=row[2]) for row in rows]
+        return actors
 
-class ActorManager(models.Manager):
-    def create_actor(self, first_name: str, last_name: str) -> Actor:
-        actor = self.create(first_name=first_name, last_name=last_name)
-        return actor
+    def update(self, actor: Actor) -> None:
+        self.cursor.execute("UPDATE actors SET first_name = ?, "
+                            "last_name = ? WHERE id = ?",
+                            (actor.first_name, actor.last_name, actor.id))
+        self.conn.commit()
 
-    def update_actor(self, actor_id: int,
-                     first_name: str = None, last_name: str = None) -> None:
-        actor = self.get(id=actor_id)
-        if first_name is not None:
-            actor.first_name = first_name
-        if last_name is not None:
-            actor.last_name = last_name
-        actor.save()
-
-    def delete_actor(self, actor_id: int) -> None:
-        actor = self.get(id=actor_id)
-        actor.delete()
+    def delete(self, actor_id: int) -> None:
+        self.cursor.execute("DELETE FROM actors WHERE id = ?",
+                            (actor_id,))
+        self.conn.commit()
