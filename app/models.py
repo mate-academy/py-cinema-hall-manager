e78c50fe321438495c42a9965000f60d2241850c
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-11-04 16:19:56 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,9 @@
-from django.db import models
+# models.py
+from dataclasses import dataclass
 
 
-class Actor(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+@dataclass
+class Actor:
+    id: int
+    first_name: str
+    last_name: str
