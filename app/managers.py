66f512ca1a8f439a4287c9a120cd9a91c9bc5629
DanSheremeta <danylo3002@gmail.com>
2023-11-28 23:58:00 +0200

    Fixed flake8

--- app/managers.py
+++ app/managers.py
@@ -4,18 +4,19 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.table_name = "actors"
         self._connection = sqlite3.connect("cinema.db3")
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -25,7 +26,12 @@ class ActorManager:
             for row in cursor
         ]
 
-    def update(self, _id: int, new_first_name: str, new_last_name:str):
+    def update(
+            self,
+            _id: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ?"
@@ -34,7 +40,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, _id: int):
+    def delete(self, _id: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} "
             f"WHERE id = ?",
