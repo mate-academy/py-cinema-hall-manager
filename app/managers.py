9186419c524121c5c4062ba5dfc03b715414bf16
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2023-12-09 19:57:38 +0200

    fix arguments and select

--- app/managers.py
+++ app/managers.py
@@ -19,20 +19,20 @@ class ActorManager:
 
     def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
-            f"SELECT id, first_name, last_name FROM {self.table_name}"
+            f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*row) for row in actors_cursor]
 
     def update(self,
                id_to_update: int,
-               new_first_name: str,
-               new_last_name: str
+               first_name: str,
+               last_name: str
                ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name}"
             " SET first_name = ?, last_name = ?"
             " WHERE id = ?",
-            (new_first_name, new_last_name, id_to_update,)
+            (first_name, last_name, id_to_update,)
         )
         self._connection.commit()
 
