0e492b83bd9335f4d6ecf82c7e2eb3bda2ff0fd7
Nataliia Povashevych <interchanal@ukr.net>
2023-12-19 19:49:24 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -7,7 +7,7 @@ from typing import List
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinema_db.db3")
+        self._connection = sqlite3.connect("cinema_db")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
@@ -29,9 +29,8 @@ class ActorManager:
                new_first_name: str,
                new_last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name}"
-            f"SET first_name = ?, last_name = ?"
-            f"WHERE id = ?",
+            f"UPDATE {self.table_name} SET first_name = ?, "
+            f"last_name = ? WHERE id = ?",
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
