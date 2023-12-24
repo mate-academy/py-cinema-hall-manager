1ae2034972a67b5625049884357834bd20989b53
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2023-12-24 20:54:41 +0200

    Solution_2

---
+++ app/managers.py
@@ -0,0 +1,47 @@
+import sqlite3
+from typing import List
+
+from app.models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self._connection: sqlite3.Connection = sqlite3.connect("cinema_db")
+        self.table_name: str = "actors"
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name}"
+            f" (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> List[Actor]:
+        actors_cursor = self._connection.execute(
+            f"SELECT id, first_name, last_name FROM {self.table_name}"
+        )
+        return [Actor(*row) for row in actors_cursor]
+
+    def update(
+            self,
+            actor_id: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name}"
+            f" SET first_name = ?, last_name = ? WHERE id = ?",
+            (new_first_name, new_last_name, actor_id)
+        )
+        self._connection.commit()
+
+    def delete(self, actor_id: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (actor_id,)
+        )
+        self._connection.commit()
+
+    def close_connection(self) -> None:
+        self._connection.close()
