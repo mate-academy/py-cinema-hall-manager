3e72b608dda96977caf77f380310d6b7dcf8450b
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2024-01-05 20:45:50 +0200

    Add app/cinema to .gitignore and delete from repository

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     # Actor.objects.create(first_name="Emma", last_name="Watson")
     # Actor.objects.create(first_name="Daniel", last_name="Radclife")
-    # print(Actor.objects.all())
+    print(Actor.objects.all())
     # Actor.objects.update(2, "Daniel", "Radcliffe")
     # print(Actor.objects.all())
     # Actor.objects.delete(2)
