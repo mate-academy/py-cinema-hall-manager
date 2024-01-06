f75af5554e9a5818d36ae639f53ffb0994baa821
Anton Konyk <antonkonyk.com>
2024-01-06 20:00:13 +0100

    Solution_06012024

--- app/managers.py
+++ app/managers.py
@@ -18,8 +18,8 @@ class ActorManager:
 
     def all(self) -> list:
         formats_cursor = (self._connection.execute
-                                   (f"SELECT id, first_name, last_name "
-                                    f"FROM {self.table_name}"))
+                          (f"SELECT id, first_name, last_name "
+                           f"FROM {self.table_name}"))
 
         return [Actor(*row) for row in formats_cursor]
 
