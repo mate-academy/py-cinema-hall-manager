7db6bed2689be418f0a9aad65851df4f0d41add4
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-01-21 15:40:45 +0200

    Solution

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Actor:
+    def __init__(
+            self,
+            id_: int,
+            first_name: str,
+            last_name: str) -> None:
+        self.id_ = id_
+        self.first_name = first_name
+        self.last_name = last_name
