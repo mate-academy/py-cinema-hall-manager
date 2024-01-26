be8d9bf64802f4f968bf9ff19ba24cde632244f6
artem.puhach <artem.puhach@cyberpollice.gov.ua>
2024-01-26 23:03:52 +0200

    fix flake

--- app/managers.py
+++ app/managers.py
@@ -4,11 +4,11 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema")
-        self.default_table = 'actors'
+        self.default_table = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.default_table} "
             "(first_name, last_name) "
@@ -17,13 +17,18 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list[Actor]:
         cursor = self._connection.execute(
             f"SELECT * FROM {self.default_table}"
         )
         return [Actor(*row) for row in cursor]
 
-    def update(self, first_name: str, last_name: str, id_to_update: int):
+    def update(
+            self,
+            first_name: str,
+            last_name: str,
+            id_to_update: int
+    ) -> None:
         self._connection.execute(
             f"UPDATE {self.default_table} "
             "SET first_name = ?, last_name = ? "
@@ -32,11 +37,10 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.default_table} "
             "WHERE id = ? ",
             (id_to_delete,)
         )
         self._connection.commit()
-
