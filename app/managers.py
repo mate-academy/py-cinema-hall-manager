496a5ae426838c29f3919584f2d188df0593f072
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-02-06 21:07:12 +0200

    Solution-3

--- app/managers.py
+++ app/managers.py
@@ -10,8 +10,8 @@ class ActorManager:
 
     def create(self: any, first_name: str, last_name: str) -> None:
         self._conn.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name)"
-            f" VALUES (?, ?)",
+            f'INSERT INTO {self.table_name} (first_name, last_name)'
+            f' VALUES (?, ?)',
             (first_name, last_name,)
         )
         self._conn.commit()
@@ -23,7 +23,7 @@ class ActorManager:
             new_last_name: str
     ) -> None:
         self._conn.execute(
-            f"UPDATE {self.table_name} "
+            f'UPDATE {self.table_name} '
             'SET first_name = ?, last_name = ? WHERE id_ = ? ',
             (new_first_name, new_last_name, id_actor,)
         )
@@ -31,14 +31,14 @@ class ActorManager:
 
     def delete(self: any, id_to_delete: int) -> None:
         self._conn.execute(
-            f"DELETE FROM {self.table_name} WHERE id_ = ? ",
+            f'DELETE FROM {self.table_name} WHERE id_ = ? ',
             (id_to_delete,)
         )
         self._conn.commit()
 
     def all(self: any) -> any:
         cinema_cursor = self._conn.execute(
-            f"SELECT * FROM {self.table_name}"
+            f'SELECT * FROM {self.table_name}'
         )
         return [
             Actor(*row) for row in cinema_cursor
