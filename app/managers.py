74b5edfa15e952650029782fd9945618698af757
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-02-06 20:19:11 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,13 +4,14 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._conn = sqlite3.connect("cinema.sqlite")
-        self.table_name = "actors1"
+        self.table_name = "actor"
 
     def create(self, first_name: str, last_name: str):
         self._conn.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name)"
+            f" VALUES (?, ?)",
             (first_name, last_name,)
         )
         self._conn.commit()
@@ -18,19 +19,19 @@ class ActorManager:
     def update(self, id_actor: int, new_first_name: str, new_last_name: str):
         self._conn.execute(
             f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ? WHERE id_actor = ? ",
+            "SET first_name = ?, last_name = ? WHERE id = ? ",
             (new_first_name, new_last_name, id_actor,)
         )
         self._conn.commit()
 
     def delete(self, id_to_delete: int):
         self._conn.execute(
-            f"DELETE FROM {self.table_name} WHERE id_actor = ? ",
+            f"DELETE FROM {self.table_name} WHERE id = ? ",
             (id_to_delete,)
         )
         self._conn.commit()
 
-    def all(self):
+    def all(self) -> None:
         cinema_cursor = self._conn.execute(
             f"SELECT * FROM {self.table_name}"
         )
