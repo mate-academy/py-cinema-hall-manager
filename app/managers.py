d58815ecbde98f80238f4c4a527b6d6dd0a10176
MaxymChyncha <max.chyncha@gmail.com>
2024-02-07 19:43:31 +0200

    Fixed update method

--- app/managers.py
+++ app/managers.py
@@ -34,8 +34,8 @@ class ActorManager:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET (first_name, last_name) = (?, ?) "
-            f"WHERE id = {id_to_update}",
-            (new_first_name, new_last_name)
+            f"WHERE id = ?",
+            (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
