46d7a0b6ced4b2761054ad60ba7cf8ff7b2dca34
yulias97 <yulkastetsko@gmail.com>
2024-02-08 17:47:02 +0200

    Fix solution

--- app/managers.py
+++ app/managers.py
@@ -5,20 +5,20 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("../cinema.sqlite")
-        self.table_name = "actor"
+        self._connection = sqlite3.connect("cinema.sqlite")
+        self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} "
-            f"(first_name, last_name) VALUES (?, ?)",
+            "(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         cursor = self._connection.execute(
-            "SELECT * FROM actor"
+            f"SELECT * FROM {self.table_name}"
         )
         return [
             Actor(*row) for row in cursor
@@ -27,14 +27,16 @@ class ActorManager:
     def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ? WHERE id = ?",
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
             (first_name, last_name, actor_id)
         )
         self._connection.commit()
 
     def delete(self, actor_id: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ?",
+            f"DELETE FROM {self.table_name} "
+            "WHERE id = ?",
             (actor_id,)
         )
         self._connection.commit()
