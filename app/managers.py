367a5bdff4aab3477976e39161e792f0917f2ea7
barabarinov <barabarinov@gmail.com>
2024-02-08 17:51:36 +0200

    fixed f-strings

--- app/managers.py
+++ app/managers.py
@@ -15,7 +15,8 @@ class ActorManager:
     ) -> None:
         self._connect.execute(
             f"INSERT INTO {self.table_name} "
-            f"(first_name, last_name) VALUES (?, ?) ",
+            "(first_name, last_name) "
+            "VALUES (?, ?) ",
             (first_name, last_name),
         )
         self._connect.commit()
@@ -28,29 +29,34 @@ class ActorManager:
     ) -> None:
         self._connect.execute(
             f"UPDATE {self.table_name} "
-            f"SET first_name = ?, last_name = ? "
-            f"WHERE id = ? ",
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ? ",
             (new_first_name, new_last_name, id_to_update),
         )
         self._connect.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connect.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ? ",
+            f"DELETE "
+            f"FROM {self.table_name} "
+            f"WHERE id = ? ",
             (id_to_delete,),
         )
         self._connect.commit()
 
     def all(self) -> [Actor]:
         actor_cursor = self._connect.execute(
-            f"SELECT * FROM {self.table_name}"
+            f"SELECT * "
+            f"FROM {self.table_name}"
         )
 
         return [Actor(*actor) for actor in actor_cursor]
 
     def get(self, id_to_get: int) -> Actor:
         actor_cursor = self._connect.execute(
-            f"SELECT * FROM {self.table_name} WHERE id = ? ",
+            f"SELECT * "
+            f"FROM {self.table_name} "
+            f"WHERE id = ? ",
             (id_to_get,),
         )
         actor_cursor = actor_cursor.fetchone()
