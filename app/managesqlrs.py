a3078acc73a4901c8ebaf239a9834499183e3215
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-02-08 16:13:35 +0200

    Task solution

---
+++ app/managesqlrs.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+from models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("../cinema.sqlite")
+        self._table_name = "actors"
+
+    def create(self,
+               first_name: str,
+               last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self._table_name} (first_name, last_name) VALUES (?, ?)",
+            (last_name, first_name)
+        )
+        self._connection.commit()
+
+    def all(self):
+        actors_cursor = self._connection.execute(
+            f"SELECT * FROM {self._table_name}"
+        )
+        return [
+            Actor(*row) for row in actors_cursor
+        ]
+
+    def update(self,
+               update_id: int,
+               first_name: str,
+               last_name: str) -> None:
+        self._connection.execute(
+            f"UPDATE {self._table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ? ",
+            (update_id, first_name, last_name)
+        )
+        self._connection.commit()
+
+    def delete(self, delete_id: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self._table_name} WHERE id = ? ",
+            (delete_id, )
+        )
+        self._connection.commit()
