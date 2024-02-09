f53dadd0074b49e2e2550da617352d9ed5837a1f
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-02-09 13:08:07 +0200

    Sol

--- app/managers.py
+++ app/managers.py
@@ -10,14 +10,17 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self.conn.execute(
-            f"INSERT INTO {self.table} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table} "
+            f"(first_name, last_name) "
+            f"VALUES (?, ?)",
             (first_name, last_name)
         )
         self.conn.commit()
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         actors_cursor = self.conn.execute(
-            f"SELECT * FROM {self.table}"
+            f"SELECT * "
+            f"FROM {self.table}"
         )
         return [Actor(*row) for row in actors_cursor]
 
@@ -26,15 +29,18 @@ class ActorManager:
                new_first_name: str,
                new_last_name: str) -> None:
         self.conn.execute(
-            f"UPDATE {self.table} SET first_name = ?,"
-            f" last_name =? WHERE id = ?",
+            f"UPDATE {self.table}"
+            f" SET first_name = ?,"
+            f" last_name =? "
+            f"WHERE id = ?",
             (id_to_update, new_first_name, new_last_name)
         )
         self.conn.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self.conn.execute(
-            f"DELETE FROM {self.table} WHERE id = ?",
+            f"DELETE FROM {self.table}"
+            f" WHERE id = ?",
             (id_to_delete,)
         )
         self.conn.commit()
