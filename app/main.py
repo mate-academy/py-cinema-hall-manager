fa89cf25e6e4853b82fed6b509ba056348a12ea3
Artem Hryshko <artemggg2222@gmail.com>
2024-02-09 17:43:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@
 #
 # if __name__ == "__main__":
 #     Actor.objects = ActorManager()
-#
-#     Actor.objects.create(first_name="Emma", last_name="Watson")
+# #
+    # Actor.objects.create(first_name="Emma", last_name="Watson")
 #     Actor.objects.create(first_name="Daniel", last_name="Radclife")
 #     print(Actor.objects.all())
 #     Actor.objects.update(2, "Daniel", "Radcliffe")
