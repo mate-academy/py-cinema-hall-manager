6c92eab3fee8ee711065fa2df80a20ced9036904
unknown <artem.sapiens@gmail.com>
2024-02-10 18:28:49 +0200

    Fix-according-to-comments

--- app/managers.py
+++ app/managers.py
@@ -9,14 +9,16 @@ class ActorManager:
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        query = (f"INSERT INTO {self.table_name} "
-                 f"(first_name, last_name) VALUES (?, ?)")
-        self._connection.execute(query, (first_name, last_name))
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+                f"(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name))
         self._connection.commit()
 
-    def all(self) -> None:
+    def all(self) -> list:
         actors_cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+            f"SELECT * "
+            f"FROM {self.table_name}"
         )
         return [
             Actor(*row) for row in actors_cursor
@@ -25,15 +27,17 @@ class ActorManager:
     def update(
             self, id_to_update: int, first_name: str, last_name: str
     ) -> None:
-        query = (f"UPDATE {self.table_name} "
-                 f"SET first_name = ?, last_name = ? "
-                 f"WHERE id = ?")
         self._connection.execute(
-            query, (first_name, last_name, id_to_update)
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = ?",
+            (first_name, last_name, id_to_update)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
-        query = f"DELETE FROM {self.table_name} WHERE id = ?"
-        self._connection.execute(query, (id_to_delete,))
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            f"WHERE id = ?",
+            (id_to_delete,))
         self._connection.commit()
