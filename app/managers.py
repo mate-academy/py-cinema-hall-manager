2bce02d4957bed2185bab31daa249561b4958231
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2024-02-18 21:23:18 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -10,7 +10,8 @@ class ActorManager:
     # CREATE 'CRUD' FUNCTIONS
     def create(self, first_name: str, last_name: str) -> None:
         self._connector.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES ('{first_name}', '{last_name}'); "
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES ('{first_name}', '{last_name}'); "
         )
         self._connector.commit()
 
@@ -20,9 +21,13 @@ class ActorManager:
 
     def update(self, index: int, f_name: str, l_name: str) -> None:
         self._connector.execute(
-            f"UPDATE {self.table_name} SET first_name = '{f_name}', last_name = '{l_name}' WHERE id = {index}; "
+            f"UPDATE {self.table_name} "
+            f"SET first_name = '{f_name}', "
+            f"last_name = '{l_name}' "
+            f"WHERE id = {index}; "
         )
         self._connector.commit()
 
     def delete(self, index: int) -> None:
-        self._connector.execute(f"DELETE FROM {self.table_name} WHERE id = {index}; ")
+        self._connector.execute(
+            f"DELETE FROM {self.table_name} WHERE id = {index}; ")
