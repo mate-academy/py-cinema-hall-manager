0f650c93c44239c7f272038bc84e5f9b336f7b59
Vitalij Chajka <chaika.vitalii@gmail.com>
2024-02-19 11:57:47 +0200

    Solution - flake8 errors

--- app/managers.py
+++ app/managers.py
@@ -3,17 +3,18 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.conn = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self.conn.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)", (first_name, last_name,)
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)", (first_name, last_name,)
         )
         self.conn.commit()
 
-    def all(self):
+    def all(self) -> list:
         actor_cursor = self.conn.execute(F"SELECT * FROM {self.table_name}")
         print(f"actor_cursor: {actor_cursor}")
         for row in actor_cursor:
@@ -22,20 +23,26 @@ class ActorManager:
             Actor(*row) for row in actor_cursor
         ]
 
-    def update(self, id_to_update: int, first_name: str, last_name: str):
+    def update(
+            self,
+            id_to_update: int,
+            first_name: str,
+            last_name: str
+    ) -> None:
         self.conn.execute(
             f"UPDATE {self.table_name} "
-            f"SET first_name = ? last_name = ? WHERE id = ?", (first_name, last_name, id_to_update,)
+            f"SET first_name = ? last_name = ? "
+            f"WHERE id = ?", (first_name, last_name, id_to_update,)
         )
         self.conn.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self.conn.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?", (id_to_delete,)
         )
         self.conn.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager = ActorManager()
     manager.all()
