aab0185f5bccea9b1335c641478efed9ac0813d0
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-02-21 17:56:00 +0200

    fix flake8 errors

--- app/managers.py
+++ app/managers.py
@@ -4,11 +4,13 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
-        self._conn = sqlite3.connect("C:/projects/py-actor-manager/cinema.sqlite")
+    def __init__(self) -> None:
+        self._conn = sqlite3.connect(
+            "C:/projects/py-actor-manager/cinema.sqlite"
+        )
         self.table_name = "actors"
 
-    def all(self):
+    def all(self) -> list:
         cinema_cursor = self._conn.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -17,14 +19,15 @@ class ActorManager:
             Actor(*row) for row in cinema_cursor
         ]
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._conn.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._conn.commit()
 
-    def update(self, actor_id: int, first_name: str, last_name: str):
+    def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self._conn.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ? "
@@ -33,7 +36,7 @@ class ActorManager:
         )
         self._conn.commit()
 
-    def delete(self, actor_id: int):
+    def delete(self, actor_id: int) -> None:
         self._conn.execute(
             f"DELETE FROM {self.table_name} WHERE id = ? ",
             (actor_id,)
