a4df327971647994ea1eb1ece63c015a85f19ba8
Molchanov Ihor <IMolchanov@kdmg.com.ua>
2024-02-27 10:29:21 +0200

    solved task

--- app/managers.py
+++ app/managers.py
@@ -5,30 +5,30 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("../cinema.sqlite")
-        self.db_table = "actors"
+        self._connection = sqlite3.connect("cinema.sqlite")
+        self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.db_table} (first_name, last_name) "
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
             f"VALUES (?, ?)", (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         actors = self._connection.execute(
-            f"SELECT * FROM {self.db_table}"
+            f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*actor) for actor in actors]
 
     def delete(self, actor_id: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.db_table} WHERE id = ?", (actor_id,)
+            f"DELETE FROM {self.table_name} WHERE id = ?", (actor_id,)
         )
 
     def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.db_table} SET first_name = ?, last_name = ? "
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
             f"WHERE id = ?", (first_name, last_name, actor_id)
         )
         self._connection.commit()
