c41379268492c726542d826acaad5b66790bd1cc
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-03-06 23:35:35 +0200

    Solution

---
+++ app/-- SQLite.sql
@@ -0,0 +1,8 @@
+-- SQLite
+CREATE TABLE IF NOT EXISTS actors (
+                id INTEGER PRIMARY KEY,
+                first_name TEXT,
+                last_name TEXT
+            );
+
+SELECT * FROM actors
