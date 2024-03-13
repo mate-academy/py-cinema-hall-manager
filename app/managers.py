7d6600c7ea7aaf896bd941abf692b90ad0ad3ea8
bujhm <Bujhm99@gmail.com>
2024-03-13 21:29:45 +0200

    AQL style, table name as atribut, removed print, uncommented

--- app/managers.py
+++ app/managers.py
@@ -4,23 +4,28 @@ import sqlite3
 class ActorManager:
     def __init__(self) -> None:
         self.connect = sqlite3.connect("../cinema.sqlite")
+        self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        sql = "Insert Into actors (first_name,last_name) VALUES (?,?)"
+        sql = f"INSERT INTO {self.table_name} " \
+              "(first_name,last_name) VALUES (?,?)"
         self.connect.execute(sql, (first_name, last_name))
         self.connect.commit()
 
     def all(self) -> None:
-        actors = self.connect.execute("Select * From actors")
-        print([actor for actor in actors])
+        actors = self.connect.execute(f"SELECT * "
+                                      f"FROM {self.table_name}")
         return actors
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
-        sql = "Update actors Set first_name = ?, last_name = ? Where id = ?"
+        sql = f"UPDATE {self.table_name} " \
+              "SET first_name = ?, last_name = ? " \
+              "WHERE id = ?"
         self.connect.execute(sql, (first_name, last_name, id))
-        # self.connect.commit()
+        self.connect.commit()
 
     def delete(self, id: int) -> None:
-        sql = "Delete From actors Where id = ?"
+        sql = f"DELETE FROM {self.table_name} " \
+              "WHERE id = ?"
         self.connect.execute(sql, (id,))
         self.connect.commit()
