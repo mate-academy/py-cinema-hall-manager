d875953ef89ee9b5be9694a48112b343752fb89b
bujhm <Bujhm99@gmail.com>
2024-04-02 08:42:28 +0300

    """

--- app/managers.py
+++ app/managers.py
@@ -6,23 +6,28 @@ class ActorManager:
         self.connect = sqlite3.connect("../cinema.sqlite")
 
     def create(self, first_name: str, last_name: str) -> None:
-        sql = "INSERT INTO actors" \
-              "(first_name,last_name) VALUES (?,?)"
+        sql = """INSERT INTO actors (first_name,last_name) VALUES (?,?)"""
         self.connect.execute(sql, (first_name, last_name))
         self.connect.commit()
 
     def all(self) -> None:
-        actors = self.connect.execute("SELECT * FROM actors")
+        actors = self.connect.execute("""SELECT * FROM actors""")
+        return actors
+
+    def get(self) -> None:
+        actors = self.connect.execute("""SELECT * FROM actors""")
         return actors
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
-        sql = "UPDATE actors " \
-              "SET first_name = ?, last_name = ? " \
-              "WHERE id = ?"
+        sql = """
+        UPDATE actors
+        SET first_name = ?, last_name = ?
+        WHERE id = ?
+        """
         self.connect.execute(sql, (first_name, last_name, id,))
         self.connect.commit()
 
     def delete(self, id: int) -> None:
-        sql = "DELETE FROM actors WHERE id = ?"
+        sql = """DELETE FROM actors WHERE id = ?"""
         self.connect.execute(sql, (id,))
         self.connect.commit()
