638ca6eb6e573bb72deddbce5bf956de38026a19
Yuliia Dudych <juliadudych@gmail.com>
2024-04-13 17:32:03 +0300

    Add create method to ActorManager class

--- app/managers.py
+++ app/managers.py
@@ -4,3 +4,10 @@ import sqlite3
 class ActorManager:
     def __init__(self) -> None:
         self._connection = sqlite3.connect("../cinema_db.sqlite")
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            "INSERT INTO actors (first_name, last_name) "
+            "VALUES (?, ?)",
+            (first_name, last_name))
+        self._connection.commit()
