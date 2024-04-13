bca32e690818e3b3fa6d919b31a37d7969f799e3
bujhm <Bujhm99@gmail.com>
2024-04-13 14:36:56 +0300

    """

--- app/managers.py
+++ app/managers.py
@@ -6,16 +6,12 @@ class ActorManager:
         self.connect = sqlite3.connect("../cinema.sqlite")
 
     def create(self, first_name: str, last_name: str) -> None:
-        sql = """INSERT INTO actors (first_name,last_name) VALUES (?,?)"""
+        sql = "INSERT INTO actors (first_name,last_name) VALUES (?,?)"
         self.connect.execute(sql, (first_name, last_name))
         self.connect.commit()
 
-    def all(self) -> None:
-        actors = self.connect.execute("""SELECT * FROM actors""")
-        return actors
-
-    def get(self) -> None:
-        actors = self.connect.execute("""SELECT * FROM actors""")
+    def all(self) -> sqlite3.Cursor:
+        actors = self.connect.execute("SELECT * FROM actors")
         return actors
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
@@ -28,6 +24,6 @@ class ActorManager:
         self.connect.commit()
 
     def delete(self, id: int) -> None:
-        sql = """DELETE FROM actors WHERE id = ?"""
+        sql = "DELETE FROM actors WHERE id = ?"
         self.connect.execute(sql, (id,))
         self.connect.commit()
