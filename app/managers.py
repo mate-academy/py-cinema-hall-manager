9b6eb3a8e6be3757219646cc64a6cd715bc8ae77
Dmytro Pukas <pukasdima2002@gmail.com>
2024-04-16 20:00:35 +0200

    Fix SQL style

--- app/managers.py
+++ app/managers.py
@@ -9,14 +9,16 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self.connection.execute(
-            "INSERT INTO actors (first_name, last_name) VALUES (?, ?)",
+            "INSERT INTO actors (first_name, last_name) "
+            "VALUES (?, ?)",
             (first_name, last_name)
         )
         self.connection.commit()
 
     def all(self) -> list:
         actors = self.connection.execute(
-            "SELECT * FROM actors"
+            "SELECT * "
+            "FROM actors"
         )
         return [Actor(*row) for row in actors]
 
@@ -24,14 +26,18 @@ class ActorManager:
                new_first_name: str,
                new_last_name: str) -> None:
         self.connection.execute(
-            "UPDATE actors SET first_name = ?, last_name = ? WHERE id = ?",
+            "UPDATE actors "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
             (new_first_name, new_last_name, id_to_update)
         )
         self.connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self.connection.execute(
-            "DELETE FROM actors WHERE id = ?",
+            "DELETE "
+            "FROM actors "
+            "WHERE id = ?",
             (id_to_delete,)
         )
         self.connection.commit()
