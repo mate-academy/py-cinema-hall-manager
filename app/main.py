48ab072564ec09bf5ad7cb15681a1834cf4a3b6c
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-04-18 21:02:27 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from managers import ActorManager
 if __name__ == "__main__":
     Actor.objects = ActorManager()
 
-    Actor.objects.create(first_name="Emma", last_name="Watson")
+    Actor.objects.create(first_name="Margot", last_name="Robbie")
     Actor.objects.create(first_name="Daniel", last_name="Radclife")
     print(Actor.objects.all())
     Actor.objects.update(2, "Daniel", "Radcliffe")
     print(Actor.objects.all())
-    Actor.objects.delete(1)
+    Actor.objects.delete(6)
     print(Actor.objects.all())
--- app/managers.py
+++ app/managers.py
@@ -3,24 +3,27 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         actor_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*row) for row in actor_cursor]
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name: str):
+    def update(self, id_to_update: int,
+               new_first_name: str,
+               new_last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ? "
@@ -29,7 +32,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?",
             (id_to_delete,)
