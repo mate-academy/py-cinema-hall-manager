9cda083300e8e9c4ef2ca15e3aa2ec6502f463a2
Yaroslav Ivanov <165368790+nrjss67@users.noreply.github.com>
2024-04-21 17:29:31 +0300

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,17 +1,17 @@
 import sqlite3
-from app.models import Actor
 
 
 class ActorManager:
 
     def __init__(self) -> None:
-        self.connection = sqlite3.connect('../cinema.sqlite')
-        self._table_name = 'actors'
+        self.connection = sqlite3.connect("../cinema.sqlite")
+        self._table_name = "actors"
 
-    def all(self) -> None:
+    def all(self) -> list:
         data = self.connection.execute(
-            f"SELECT * FROM {self._table_name};"
+            f"SELECT * FROM {self._table_name} "
         )
+
         return [actor for actor in data]
 
     def create(self, first_name: str, last_name: str) -> None:
@@ -21,16 +21,17 @@ class ActorManager:
         )
         self.connection.commit()
 
-    def update(self, id: int, first_name: str, last_name: str) -> None:
+    def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self.connection.execute(
-            f"UPDATE {self._table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self._table_name} SET first_name=?, "
+            f"last_name=? WHERE actor_id=?",
             (first_name, last_name, id)
         )
         self.connection.commit()
 
     def delete(self, id: int) -> None:
         self.connection.execute(
-            f"DELETE FROM {self._table_name} WHERE id=?",
+            f"DELETE FROM {self._table_name} WHERE actor_id=?",
             (id,)
         )
         self.connection.commit()
