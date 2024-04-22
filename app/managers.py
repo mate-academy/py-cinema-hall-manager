0ebf78e0a84d1e6eebe9a71f64d72b491c775c00
MRGL <Sonemongoo@gmail.com>
2024-04-23 00:09:49 +0300

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -3,13 +3,14 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?,?)",
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name),
         )
         self._connection.commit()
@@ -20,9 +21,13 @@ class ActorManager:
         )
         return [Actor(*row) for row in cursor]
 
-    def update(self, id_to_update: int, new_name: str, new_last_name: str) -> None:
+    def update(self,
+               id_to_update: int,
+               new_name: str,
+               new_last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name =?, last_name =? WHERE id =?",
+            f"UPDATE {self.table_name} "
+            "SET first_name =?, last_name =? WHERE id =?",
             (new_name, new_last_name, id_to_update),
         )
 
