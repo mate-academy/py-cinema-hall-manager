e21de2618fe9909f30cba08c0c4752430fcaa4b3
Artur <galstan727@gmail.com>
2024-05-01 13:15:24 +0300

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -8,22 +8,22 @@ class ActorManager:
         self.cursor = self.connection.cursor()
 
     def create(self, first_name: str, last_name: str) -> None:
-        self.cursor.execute('''
-            INSERT INTO actors (first_name, last_name) VALUES (?, ?)
-        ''', (first_name, last_name))
         self.connection.commit()
 
     def all(self) -> list:
-        self.cursor.execute('SELECT * FROM actors')
+        self.cursor.execute("SELECT * FROM actors")
         rows = self.cursor.fetchall()
         return [Actor(*row) for row in rows]
 
-    def update(self, actor_id: int, new_first_name: str, new_last_name: str) -> None:
-        self.cursor.execute('''
+    def update(self,
+               actor_id: int,
+               new_first_name: str,
+               new_last_name: str) -> None:
+        self.cursor.execute("""
             UPDATE actors SET first_name=?, last_name=? WHERE id=?
-        ''', (new_first_name, new_last_name, actor_id))
+        """, (new_first_name, new_last_name, actor_id))
         self.connection.commit()
 
     def delete(self, actor_id: int) -> None:
-        self.cursor.execute('DELETE FROM actors WHERE id=?', (actor_id,))
+        self.cursor.execute("DELETE FROM actors WHERE id=?", (actor_id,))
         self.connection.commit()
