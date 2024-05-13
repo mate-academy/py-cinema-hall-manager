ba223408e3c1832bc3279ce21dd59ca95e2d26e6
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-05-14 00:40:11 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -7,17 +7,18 @@ class ActorManager:
     def __init__(self) -> None:
         db_path = os.path.join("..", "cinema.sqlite")
         self._connection = sqlite3.connect(db_path)
+        self.table_name = "actors "
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO actors (first_name, last_name) VALUES (?, ?) ",
+            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?) ",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute(
-            f"SELECT * FROM actors "
+            f"SELECT * FROM {self.table_name} "
         )
         return [Actor(*actor) for actor in actor_cursor]
 
@@ -26,7 +27,7 @@ class ActorManager:
                new_f_name: str,
                new_l_name: str) -> None:
         self._connection.execute(
-            f"UPDATE actors "
+            f"UPDATE {self.table_name} "
             "SET (first_name, last_name) = (?, ?) "
             "WHERE id = ? ",
             (new_f_name, new_l_name, id_to_update)
@@ -35,7 +36,7 @@ class ActorManager:
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE FROM actors WHERE id = ? ",
+            f"DELETE FROM {self.table_name} WHERE id = ? ",
             (id_to_delete,)
         )
         self._connection.commit()
