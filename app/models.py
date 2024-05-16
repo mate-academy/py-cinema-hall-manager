f38aeb976c951d272328e0365e03dd54e4afa9b9
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-05-16 19:40:10 +0300

    finally result

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Actor:
-    def __init__(self, id: int, first_name: str, last_name: str):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
+    id: int
+    first_name: str
+    last_name: str
