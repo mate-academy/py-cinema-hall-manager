f121d8aa65330e7a6757e3a422e1e24567ae96ec
onyevyerov <onyevyerov@gmail.com>
2024-05-22 20:33:54 +0200

    Changed return type annotation

--- app/managers.py
+++ app/managers.py
@@ -14,7 +14,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
             "SELECT * FROM cinema.sqlite.actors"
         )
