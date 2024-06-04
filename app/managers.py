bb88cf07a3f0c0f81594f48349ea89b757cacf70
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-06-04 17:47:35 +0300

    fixed flake8 errors

--- app/managers.py
+++ app/managers.py
@@ -4,26 +4,32 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES (?, ?)",
             (first_name, last_name),
         )
 
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
 
         return [Actor(*row) for row in actors_cursor]
 
-    def update(self, id_to_update, first_name, last_name):
+    def update(
+            self,
+            id_to_update: int,
+            first_name: str,
+            last_name: str
+    ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ? "
@@ -33,11 +39,10 @@ class ActorManager:
 
         self._connection.commit()
 
-    def delete(self, id_to_delete):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?",
             (id_to_delete, ),
         )
 
         self._connection.commit()
-
