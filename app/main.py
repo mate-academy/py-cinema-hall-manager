4f457d5f812b6c4590f18502aa49bd51b9a02d93
andriy-py <andriy.omelya@gmail.com>
2024-06-18 12:32:45 +0300

    Done

--- app/main.py
+++ app/main.py
@@ -4,24 +4,24 @@ from managers import ActorManager
 def main() -> None:
     manager = ActorManager()
 
-    # Створення акторів
+    # creations actor
     actor_id1 = manager.create("Роберт", "Дауні-молодший")
     actor_id2 = manager.create("Кріс", "Гемсворт")
 
-    # Список всіх акторів
+    # list  all actors
     actors = manager.all()
     print("Всі актори:")
     for actor in actors:
         print(actor)
 
-    # Оновлення актора
+    # update actor
     manager.update(actor_id1, first_name="Роберт", last_name="Дауні")
     updated_actors = manager.all()
     print("\nОновлений актор:")
     for actor in updated_actors:
         print(actor)
 
-    # Видалення актора
+    # delete actor
     manager.delete(actor_id2)
     remaining_actors = manager.all()
     print("\nЗалишилися актори після видалення:")
