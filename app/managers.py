0dd2dde1724cc60a1feba684d15fc639bbbd1f02
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-06-19 16:16:36 +0200

    Done

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,6 @@
 import sqlite3
+
 from models import Actor
-from typing import Any
 
 
 class ActorManager:
@@ -8,40 +8,25 @@ class ActorManager:
         self.conn = sqlite3.connect("identifier.sqlite")
         self.cursor = self.conn.cursor()
 
-    def create(self, first_name: str, last_name: str) -> Any:
-        query = ("INSERT INTO actor_manager (first_name, last_name)"
-                 " VALUES (?, ?)")
-        self.cursor.execute(query, (first_name, last_name))
+    def create(self, first_name: str, last_name: str) -> int:
+        self.cursor.execute(
+            "INSERT INTO actor_manager (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
         self.conn.commit()
         return self.cursor.lastrowid
 
-    def all(self) -> Any:
-        query = ("SELECT id, first_name, last_name "
-                 "FROM actor_manager")
-        self.cursor.execute(query)
-        results = self.cursor.fetchall()
-        return [
-            Actor(id=row[0],
-                  first_name=row[1],
-                  last_name=row[2]) for row in results
-        ]
+    def all(self) -> list:
+        self.cursor.execute("SELECT id, first_name, last_name FROM actor_manager")
+        return [Actor(*row) for row in self.cursor.fetchall()]
 
-    def update(
-            self,
-            actor_id: int,
-            first_name: str,
-            last_name: str
-    ) -> Any:
-        query = ("UPDATE actor_manager "
-                 "SET first_name = ?, last_name = ? "
-                 "WHERE id = ?")
+    def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self.cursor.execute(
-            query,
+            "UPDATE actor_manager SET first_name = ?, last_name = ? WHERE id = ?",
             (first_name, last_name, actor_id)
         )
         self.conn.commit()
 
-    def delete(self, actor_id: int) -> Any:
-        query = "DELETE FROM actor_manager WHERE id = ?"
-        self.cursor.execute(query, (actor_id,))
+    def delete(self, actor_id: int) -> None:
+        self.cursor.execute("DELETE FROM actor_manager WHERE id = ?", (actor_id,))
         self.conn.commit()
