98d89519bf6ed7edeecff7b963117e21cea9aa80
jpdreamthug <lentorilp@gmail.com>
2024-06-20 21:56:29 +0300

    rename variables

--- app/managers.py
+++ app/managers.py
@@ -19,22 +19,22 @@ class ActorManager:
         self._connection.commit()
 
     def all(self) -> list:
-        cinema_actors_cursor = self._connection.execute(
+        actors = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
         return [
-            Actor(*row) for row in cinema_actors_cursor
+            Actor(*row) for row in actors
         ]
 
     def update(self,
-               id_to_update: int,
+               actor_id: int,
                new_first_name: str,
                new_last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ? "
             f"WHERE id = ?",
-            (new_first_name, new_last_name, id_to_update)
+            (new_first_name, new_last_name, actor_id)
         )
         self._connection.commit()
 
