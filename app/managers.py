f29e38c022df15120106eca7b2a0dcaaba18a74f
TkachukTara <zaz42988@gmail.com>
2024-06-21 09:56:00 +0300

    Solved task? fix sql style

--- app/managers.py
+++ app/managers.py
@@ -10,28 +10,33 @@ class ActorManager:
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} (first_name, last_name) "
-            f"VALUES (?, ?)",
+            "VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+            f"SELECT * "
+            f"FROM {self.table_name}"
         )
         return [Actor(*row) for row in cursor]
 
-    def update(self, id_to_update: int,
-               new_first_name: str, new_last_name: str) -> None:
+    def update(self, id_to_update: int, new_first_name: str,
+               new_last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
-            f"SET first_name=?, last_name=? WHERE id=?",
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id=?", (id_to_delete,)
+            f"DELETE "
+            f"FROM {self.table_name} "
+            "WHERE id = ?",
+            (id_to_delete,)
         )
         self._connection.commit()
