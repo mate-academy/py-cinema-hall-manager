103aab2d1d74f45196a0c010328593ff92109ac4
Halyna Rohatska <rohatskahr@gmail.com>
2024-07-01 18:35:54 +0300

    fix type annotation

--- app/managers.py
+++ app/managers.py
@@ -40,7 +40,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ? ",
             (id_to_delete,)
