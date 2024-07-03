9788737cb2749de50d0307ed215a21cbd76739a2
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-03 13:10:47 +0300

    Solution 03-07-2024 13-10

---
+++ app/managers.py
@@ -0,0 +1,15 @@
+class ActorManager:
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def all(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
