d4d453b080252fc73bf3ff6a9ff826e99a2f933a
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-03 20:21:53 +0300

    Solution 03-07-2024 20-21

--- app/managers.py
+++ app/managers.py
@@ -4,14 +4,14 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actor"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} "
-            f"(first_name, last_name) VALUES (?,?)",
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name,)
         )
         self._connection.commit()
@@ -24,14 +24,20 @@ class ActorManager:
             Actor(*row) for row in actor_cursor
         ]
 
-    def update(self, id_to_update: int, first_name: str, last_name: str) -> None:
+    def update(self,
+               id_to_update: int,
+               first_name: str,
+               last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ?"
             f"WHERE id = ? ",
-            (first_name,last_name, id_to_update)
+            (first_name, last_name, id_to_update)
         )
         self._connection.commit()
 
-    def delete(self):
-        pass
+    def delete(self, id_to_delete: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ? ", (id_to_delete,)
+        )
+        self._connection.commit()
