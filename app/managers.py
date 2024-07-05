f2cb3ae0d525dbcc1ed7da8c52a8ec08cd69f12f
Viktor Shcherbak <dev.psychologist@gmail.com>
2024-07-05 16:12:34 +0300

    DB CRUD created!

---
+++ app/managers.py
@@ -0,0 +1,42 @@
+import sqlite3
+from models import Actor
+from pathlib import Path
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("/Users/szlubny/PycharmProjects/NLP/py-actor-manager/cinema_db.sqlite")
+        self.table_name = "actors"
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> list[Actor]:
+        actor_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}",
+        )
+
+        return [Actor(*row) for row in actor_cursor]
+
+    def update(self,
+               id_update: int,
+               new_first_name: str,
+               new_last_name: str) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? WHERE id = ?",
+            (new_first_name, new_last_name, id_update)
+        )
+        self._connection.commit()
+
+    def delete(self, id_del: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (id_del,)
+        )
+        self._connection.commit()
