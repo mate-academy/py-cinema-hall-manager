e652b42a0167e650ecfa85eb3d93d38116ada623
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-07-14 16:03:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,31 @@
-# from models import Actor
-# from managers import ActorManager
-#
-# if __name__ == "__main__":
-#     Actor.objects = ActorManager()
-#
-#     Actor.objects.create(first_name="Emma", last_name="Watson")
-#     Actor.objects.create(first_name="Daniel", last_name="Radclife")
-#     print(Actor.objects.all())
-#     Actor.objects.update(2, "Daniel", "Radcliffe")
-#     print(Actor.objects.all())
-#     Actor.objects.delete(1)
-#     print(Actor.objects.all())
+from managers import ActorManager
+
+
+def main():
+    manager = ActorManager()
+
+    # Створення нових акторів
+    manager.create('Leonardo', 'DiCaprio')
+    manager.create('Morgan', 'Freeman')
+
+    # Отримання всіх акторів
+    actors = manager.all()
+    print("Actors in database:")
+    for actor in actors:
+        print(actor)
+
+    # Оновлення інформації про актора
+    manager.update(1, new_first_name='Leonard', new_last_name='DiCaprio')
+
+    # Видалення актора
+    manager.delete(2)
+
+    # Отримання всіх акторів після оновлення та видалення
+    actors = manager.all()
+    print("Actors in database after update and delete:")
+    for actor in actors:
+        print(actor)
+
+
+if __name__ == '__main__':
+    main()
