a3ea6dca3684b12ab5f46862edb42412b1f606af
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-07-14 16:30:30 +0200

    fix errors

--- app/main.py
+++ app/main.py
@@ -4,23 +4,18 @@ from managers import ActorManager
 def main() -> None:
     manager = ActorManager()
 
-    # Створення нових акторів
     manager.create("Leonardo", "DiCaprio")
     manager.create("Morgan", "Freeman")
 
-    # Отримання всіх акторів
     actors = manager.all()
     print("Actors in database:")
     for actor in actors:
         print(actor)
 
-    # Оновлення інформації про актора
     manager.update(1, new_first_name="Leonard", new_last_name="DiCaprio")
 
-    # Видалення актора
     manager.delete(2)
 
-    # Отримання всіх акторів після оновлення та видалення
     actors = manager.all()
     print("Actors in database after update and delete:")
     for actor in actors:
