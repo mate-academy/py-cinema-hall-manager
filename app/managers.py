de9b7ef87c23f79f3282a235d6d29d43f3db999b
Your_Name Your_Surname <your@email>
2024-07-26 15:47:49 +0300

    add chenges

--- app/managers.py
+++ app/managers.py
@@ -35,7 +35,10 @@ class ActorManager:
             f"UPDATE {self.table_name}"
             " SET first_name = ?, last_name = ? "
             "WHERE id = ?",
-            (new_first_name, new_last_name, id_to_update)
+            (
+                new_first_name,
+                new_last_name,
+                id_to_update)
         )
         self._connection.commit()
 
