636fd05061f9017d937d286daf3371d2b4a9387a
DaniaK123 <152022387+DaniaK123@users.noreply.github.com>
2024-07-28 01:15:45 +0300

    Update create_db.py

--- app/create_db.py
+++ app/create_db.py
@@ -1,19 +1,1 @@
-import sqlite3
 
-def create_database():
-    connection = sqlite3.connect('cinema.db')
-    cursor = connection.cursor()
-    
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS actors (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        first_name TEXT NOT NULL,
-        last_name TEXT NOT NULL
-    )
-    ''')
-    
-    connection.commit()
-    connection.close()
-
-if __name__ == "__main__":
-    create_database()
