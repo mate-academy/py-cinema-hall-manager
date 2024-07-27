565589c74076e77caf36fd6606c96942e9ba2551
DaniaK123 <152022387+DaniaK123@users.noreply.github.com>
2024-07-28 01:13:46 +0300

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,50 +1,50 @@
 import sqlite3
+
 from models import Actor
 
+
 class ActorManager:
-    def __init__(self):
-        self.connection = sqlite3.connect('cinema.db')
-        self.cursor = self.connection.cursor()
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("../cinema.sqlite")
+        self.table_name = "actors"
 
-    def create(self, first_name, last_name):
-        self.cursor.execute('''
-        INSERT INTO actors (first_name, last_name)
-        VALUES (?, ?)
-        ''', (first_name, last_name))
-        self.connection.commit()
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
+            (first_name, last_name),
+        )
+        self._connection.commit()
 
-    def all(self):
-        self.cursor.execute('SELECT id, first_name, last_name FROM actors')
-        rows = self.cursor.fetchall()
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+    def all(self) -> list[Actor]:
+        actors_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+        return [
+            Actor(*row) for row in actors_cursor
+        ]
 
-    def update(self, actor_id, first_name=None, last_name=None):
-        if first_name and last_name:
-            self.cursor.execute('''
-            UPDATE actors
-            SET first_name = ?, last_name = ?
-            WHERE id = ?
-            ''', (first_name, last_name, actor_id))
-        elif first_name:
-            self.cursor.execute('''
-            UPDATE actors
-            SET first_name = ?
-            WHERE id = ?
-            ''', (first_name, actor_id))
-        elif last_name:
-            self.cursor.execute('''
-            UPDATE actors
-            SET last_name = ?
-            WHERE id = ?
-            ''', (last_name, actor_id))
-        self.connection.commit()
+    def update(
+            self, id_to_update: int, first_name: str, last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ? ",
+            (first_name, last_name, id_to_update)
+        )
+        self._connection.commit()
 
-    def delete(self, actor_id):
-        self.cursor.execute('''
-        DELETE FROM actors
-        WHERE id = ?
-        ''', (actor_id,))
-        self.connection.commit()
+    def delete(self, id_to_delete: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            "WHERE id = ? ",
+            (id_to_delete,)
+        )
+        self._connection.commit()
 
-    def __del__(self):
-        self.connection.close()
+
+if __name__ == "__main__":
+    manager = ActorManager()
+    manager.all()
+    print(manager.all())
