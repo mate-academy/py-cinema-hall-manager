d43a44dca15f4decf2733381a766b5c89b788ee1
Vasyl <Widbmak@gmail.com>
2024-08-17 19:47:38 +0300

    test4

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from managers import ActorManager
 if __name__ == "__main__":
     Actor.objects = ActorManager()
     Actor.objects.create(first_name="bababb", last_name="weweewe")
-    # Actor.objects.create(first_name="Daniel", last_name="Radclife")
-    # print(Actor.objects.all())
-    # Actor.objects.update(2, first_name="Daniel", last_name="Radcliffe")
+    Actor.objects.create(first_name="Daniel", last_name="Radclife")
+    print(Actor.objects.all())
+    Actor.objects.update(2, first_name="Daniel", last_name="Radcliffe")
     print(Actor.objects.all())
     Actor.objects.delete(24)
-    # print(Actor.objects.all())
+    print(Actor.objects.all())
