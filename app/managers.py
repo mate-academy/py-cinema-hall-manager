bf29fe6ef8ce71bc1a57d424c6783147ee2e9a11
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-20 15:28:40 +0200

    implement py-actor-manager

--- app/managers.py
+++ app/managers.py
@@ -4,18 +4,19 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
-        self._connection = sqlite3.connect('cinema.sqlite')
-        self.table_name = 'actors'
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema.sqlite")
+        self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name,)
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         actors_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -23,7 +24,12 @@ class ActorManager:
             Actor(*row) for row in actors_cursor
         ]
 
-    def update(self, id_actor: int, actor_fist_name: str, actor_last_name: str):
+    def update(
+            self,
+            id_actor: int,
+            actor_fist_name: str,
+            actor_last_name: str
+    ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name=?, last_name=? "
@@ -32,12 +38,10 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_actor: int):
+    def delete(self, id_actor: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} "
             f"WHERE id=?",
             (id_actor,)
         )
         self._connection.commit()
-
-
