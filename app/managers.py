dad95ad93b3e2a9295cb1c9132bb440072618f1d
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-08-23 12:11:42 +0300

    'fixed'

--- app/managers.py
+++ app/managers.py
@@ -9,8 +9,8 @@ class ActorManager:
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute("""
-        INSERT INTO actors (first_name, last_name)
+        self._connection.execute(f"""
+        INSERT INTO {self.table_name} (first_name, last_name)
         VALUES (?, ?)
         """, (first_name, last_name))
 
--- app/models.py
+++ app/models.py
@@ -6,27 +6,3 @@ class Actor:
     id: int | None
     first_name: str
     last_name: str
-
-
-# Create a database cinema where will be stored
-# entries with data about different actors and actresses.
-# Create a table actors with corresponding columns.
-#
-# import sqlite3
-#
-#
-# def create_database() -> None:
-#     _connection = sqlite3.connect("cinema.db")
-#
-#     _connection.cursor().execute('''
-#     CREATE TABLE IF NOT EXISTS actors (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         first_name TEXT NOT NULL,
-#         last_name TEXT NOT NULL
-#     )
-#     ''')
-#
-#     _connection.commit()
-#     _connection.close()
-#
-# create_database()
