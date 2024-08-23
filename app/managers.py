689785038eaeaf9c1d6d7d3c8c5f90fef0aea14d
Andrii <andrii.khamaza0@gmail.com>
2024-08-23 15:46:05 +0300

    fixed flake mistakes

--- app/managers.py
+++ app/managers.py
@@ -10,7 +10,7 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"""INSERT INTO {self.table_name} 
+            f"""INSERT INTO {self.table_name}
             (first_name, last_name) VALUES (?, ?)""",
             (first_name, last_name)
         )
@@ -34,8 +34,8 @@ class ActorManager:
         self._connection.execute(
 
 
-            f"""UPDATE {self.table_name} 
-            SET first_name = ?, last_name = ? 
+            f"""UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
             WHERE id = ?""",
             (first_name, last_name, id_to_update)
         )
