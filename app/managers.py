e3a0449bb7014bf17c6ab7a6f0daf7fec8df79cf
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-23 15:00:33 +0300

    fixed solution

--- app/managers.py
+++ app/managers.py
@@ -34,9 +34,10 @@ class ActorManager:
             new_last_name: str
     ) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name}"
-            f" SET first_name = ?, last_name = ? "
-            f"WHERE id = ?",
+            f"""UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?
+            """,
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
