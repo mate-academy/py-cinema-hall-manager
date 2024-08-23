414204a8928fa497ce2d32c8a65f76dc82e1ced4
Anton Bilous <anton.bilosus@gmail.com>
2024-08-23 10:56:23 +0300

    Use triple quotes

--- app/managers.py
+++ app/managers.py
@@ -11,9 +11,10 @@ class ActorManager:
         self._connection = sqlite3.connect(self.FILE_NAME)
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute(
-            f"INSERT INTO {self.TABLE_NAME}"
-            " (first_name, last_name) VALUES (?, ?)",
+        self._connection.execute(f"""
+            INSERT INTO {self.TABLE_NAME}
+            (first_name, last_name) VALUES (?, ?)
+        """,
             (first_name, last_name),
         )
         self._connection.commit()
@@ -25,18 +26,20 @@ class ActorManager:
         return [Actor(*row) for row in cursor]
 
     def update(self, id_: int, first_name: str, last_name: str) -> None:
-        self._connection.execute(
-            f"UPDATE {self.TABLE_NAME}"
-            " SET first_name = ?, last_name = ?"
-            " WHERE id = ?",
+        self._connection.execute(f"""
+                UPDATE {self.TABLE_NAME}
+                SET first_name = ?, last_name = ?
+                WHERE id = ?
+            """,
             (first_name, last_name, id_),
         )
         self._connection.commit()
 
     def delete(self, id_: int) -> None:
-        self._connection.execute(
-            f"DELETE FROM {self.TABLE_NAME}"
-            " WHERE id = ?",
+        self._connection.execute(f"""
+                DELETE FROM {self.TABLE_NAME}
+                WHERE id = ?
+            """,
             (id_,),
         )
         self._connection.commit()
