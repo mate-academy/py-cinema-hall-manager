2934f270604358bfb89b48d6a7f4879ca0ee3336
Olexandr Bastov <iamlucky1990@gmail.com>
2024-08-23 11:10:57 +0300

    f""" """ instead of f"" for SQL queries
    full_path to the db file has been removed BUT, without it in community edition somehow code doesnt work :\ and it works just fine in Pycharm Pro

--- app/managers.py
+++ app/managers.py
@@ -1,4 +1,3 @@
-import os
 import sqlite3
 
 from models import Actor
@@ -6,36 +5,44 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self.path = os.getcwd() + os.sep + "cinema.db"
         self.table_name = "actors"
-        self._connection = sqlite3.connect(self.path)
+        self._connection = sqlite3.connect("cinema.db")
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} "
-            "(first_name, last_name) VALUES (?, ?)",
-            (first_name, last_name)
+            f"""
+            INSERT INTO {self.table_name} 
+            (first_name, last_name) VALUES (?, ?)
+            """
+            , (first_name, last_name)
+
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+            f"""
+            SELECT * FROM {self.table_name}
+            """
         )
         return [Actor(*row) for row in actor_cursor]
 
     def update(self, id_: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ? "
-            "WHERE id = ? ",
-            (last_name, first_name, id_)
+            f"""
+            UPDATE {self.table_name} 
+            SET first_name = ?, last_name = ? 
+            WHERE id = ? 
+            """
+            , (last_name, first_name, id_)
         )
         self._connection.commit()
 
     def delete(self, id_: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ? ",
-            (id_,)
+            f"""
+            DELETE FROM {self.table_name} WHERE id = ? 
+            """
+            , (id_,)
         )
         self._connection.commit()
