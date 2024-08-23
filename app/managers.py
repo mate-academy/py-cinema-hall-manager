f81ff3db5d381e2263b4086ee1ece7bcda0e9989
olena4reunited <olenaduringintermission@gmail.com>
2024-08-23 12:13:26 +0300

    fix: indentation, .

--- app/managers.py
+++ app/managers.py
@@ -13,44 +13,43 @@ class ActorManager:
         with self._connection:
             self._connection.execute(
                 f"""
-            CREATE TABLE IF NOT EXISTS {self.table_name} (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL
-            )
-            """
+                CREATE TABLE IF NOT EXISTS {self.table_name} (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    first_name TEXT NOT NULL,
+                    last_name TEXT NOT NULL
+                )
+                """
             )
 
     def create(self, first_name: str, last_name: str) -> None:
         with self._connection:
             self._connection.execute(
                 f"""
-                    INSERT INTO {self.table_name} (first_name, last_name)
-                    VALUES (?, ?)
+                INSERT INTO {self.table_name} (first_name, last_name)
+                VALUES (?, ?)
                 """,
-                (first_name, last_name),
+                (first_name, last_name)
             )
 
     def all(self) -> list[Actor]:
         with self._connection:
             read_actors = self._connection.execute(
                 f"""
-            SELECT *
-            FROM {self.table_name}
-            """
+                SELECT *
+                FROM {self.table_name}
+                """
             )
-
-        return [Actor(*row) for row in read_actors]
+        return [Actor(*row) for row in read_actors.fetchall()]
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
         with self._connection:
             self._connection.execute(
                 f"""
-                    UPDATE {self.table_name}
-                    SET first_name = ?, last_name = ?
-                    WHERE id = ?
+                UPDATE {self.table_name}
+                SET first_name = ?, last_name = ?
+                WHERE id = ?
                 """,
-                (first_name, last_name, id),
+                (first_name, last_name, id)
             )
 
     def delete(self, id: int) -> None:
@@ -60,5 +59,5 @@ class ActorManager:
                 DELETE FROM {self.table_name}
                 WHERE id = ?
                 """,
-                (id,),
+                (id,)
             )
