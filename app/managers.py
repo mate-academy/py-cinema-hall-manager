1d6b00c400c6a337ab6ca63fb8d4b966e2018a89
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-23 14:38:14 +0300

    fixed managers.py

--- app/managers.py
+++ app/managers.py
@@ -10,15 +10,15 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f'''INSERT INTO {self.table_name} 
-            (first_name, Last_name) VALUES (?, ?)''',
+            f"""INSERT INTO {self.table_name}
+            (first_name, Last_name) VALUES (?, ?)""",
             (first_name, last_name,)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute(
-            f'''SELECT * FROM {self.table_name}'''
+            f"""SELECT * FROM {self.table_name}"""
         )
         return [
             Actor(*row) for row in actor_cursor
@@ -31,16 +31,16 @@ class ActorManager:
             new_last_name: str
     ) -> None:
         self._connection.execute(
-            f'''UPDATE {self.table_name} 
-            SET (first_name, last_name) = (?, ?) 
-            WHERE id = ?''',
+            f"""UPDATE {self.table_name}
+            SET (first_name, last_name) = (?, ?)
+            WHERE id = ?""",
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f'''DELETE FROM {self.table_name} WHERE id = ?''',
+            f"""DELETE FROM {self.table_name} WHERE id = ?""",
             (id_to_delete,)
         )
         self._connection.commit()
