b1eab219fa380b44e702fb89f9be0aee599c2b6e
Alexandr Politov <apolitov83@gmail.com>
2024-08-23 12:10:17 +0300

    fixing indentations

--- app/managers.py
+++ app/managers.py
@@ -11,8 +11,7 @@ class ActorManager:
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(f"""
             INSERT INTO {self.table} (first_name, last_name)
-            VALUES (?, ?)
-""", (first_name, last_name)
+            VALUES (?, ?)""", (first_name, last_name)
         )
         self._connection.commit()
 
@@ -28,15 +27,13 @@ class ActorManager:
         self._connection.execute(f"""
         UPDATE {self.table}
         SET first_name = ?, last_name = ?
-        WHERE id = ?
-""", (first_name, last_name, id_updated)
+        WHERE id = ?""", (first_name, last_name, id_updated)
         )
         self._connection.commit()
 
     def delete(self, id_delete: int) -> None:
         self._connection.execute(f"""
         DELETE FROM {self.table}
-        WHERE id = ?
-""", (id_delete,)
+        WHERE id = ?""", (id_delete,)
         )
         self._connection.commit()
