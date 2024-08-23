e300d98160d5feb6575b7f5ae9e406baf39c0a01
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-23 17:45:45 +0300

    implement py-actor-manager(in 22 impossible to put on different lines 'cause flake8')

--- app/managers.py
+++ app/managers.py
@@ -5,17 +5,21 @@ from models import Actor
 class CinemaManager:
     def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
+        self.table_name = "Actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            "INSERT INTO Actors (first_name, last_name) VALUES (?, ?)",
+            f"""
+            INSERT INTO {self.table_name} (first_name, last_name) 
+            VALUES (?, ?)
+            """,
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         cinema_cursor = self._connection.execute(
-            "SELECT * FROM Actors"
+            f"""SELECT * FROM {self.table_name}"""
         )
         actors = [Actor(*row) for row in cinema_cursor]
         cinema_cursor.close()
@@ -23,26 +27,24 @@ class CinemaManager:
 
     def update(self, _id: int, new_name: str, new_last_name: str) -> None:
         self._connection.execute(
-            "UPDATE Actors SET first_name = ?, last_name = ? WHERE id = ?",
+            f"""
+            UPDATE {self.table_name} 
+            SET first_name = ?, last_name = ? 
+            WHERE id = ?
+            """,
             (new_name, new_last_name, _id)
         )
         self._connection.commit()
 
     def delete(self, _id: int) -> None:
         self._connection.execute(
-            "DELETE FROM Actors WHERE id = ?",
+            f"""
+            DELETE FROM {self.table_name} 
+            WHERE id = ?
+            """,
             (_id,)
         )
+        self._connection.commit()
 
     def disconnect(self) -> None:
         self._connection.close()
-
-
-if __name__ == "__main__":
-    manager = CinemaManager()
-    manager.create("Taras", "Bulba")
-    print(manager.all())
-    manager.update(1, "Petya", "Poroh")
-    print(manager.all())
-    manager.delete(1)
-    manager.disconnect()
