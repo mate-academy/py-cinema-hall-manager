659141a3a8be99f0c16dac5397b3da2062dcde13
Alexandr Politov <apolitov83@gmail.com>
2024-08-23 09:43:06 +0300

    changes SQL format

--- app/managers.py
+++ app/managers.py
@@ -9,13 +9,14 @@ class ActorManager:
         self.table = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute(
-            f"INSERT INTO {self.table} (first_name, last_name) VALUES (?, ?)",
-            (first_name, last_name)
+        self._connection.execute(f"""
+            INSERT INTO {self.table} (first_name, last_name) 
+            VALUES (?, ?)
+""", (first_name, last_name)
         )
         self._connection.commit()
 
-    def all(self) -> list:
+    def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute(
             f"SELECT * FROM {self.table}"
         )
@@ -24,17 +25,20 @@ class ActorManager:
         ]
 
     def update(self, id_updated: int, first_name: str, last_name: str) -> None:
-        self._connection.execute(
-            f"UPDATE {self.table} "
-            f"SET first_name = ?, last_name = ? "
-            f"WHERE id = ?",
+        self._connection.execute(f"""
+        UPDATE {self.table}
+        SET first_name = ?, last_name = ? 
+        WHERE id = ?
+""",
             (first_name, last_name, id_updated)
         )
         self._connection.commit()
 
     def delete(self, id_delete: int) -> None:
-        self._connection.execute(
-            f"DELETE FROM {self.table} WHERE id = ?",
+        self._connection.execute(f"""
+        DELETE FROM {self.table} 
+        WHERE id = ?
+""",
             (id_delete,)
         )
         self._connection.commit()
