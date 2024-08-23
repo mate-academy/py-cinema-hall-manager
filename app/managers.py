1b16e124b5f2a20e4fcd05c2a5fc770f9a794d29
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-23 21:14:19 +0300

    commit

--- app/managers.py
+++ app/managers.py
@@ -24,7 +24,8 @@ class ActorManager:
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f""""
-            "INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)""",
+            "INSERT INTO {self.table_name}
+             (first_name, last_name) VALUES (?, ?)""",
             (first_name, last_name)
         )
         self._connection.commit()
