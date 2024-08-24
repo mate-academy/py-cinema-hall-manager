28aaaa112499ec2d52bca54d765f8e12c0ccd5e9
Vlada <vlada.belayy@mail.com>
2024-08-24 14:34:02 +0300

    fixed2

--- app/managers.py
+++ app/managers.py
@@ -22,7 +22,9 @@ class ActorManager:
 
     def all(self) -> None:
         actor_manager_cursor = self._connection.execute(
-            f"""SELECT * FROM {self.table_name}"""
+            f"""
+            SELECT * FROM {self.table_name}
+            """
         )
         return [
             Actor(*row) for row in actor_manager_cursor
@@ -35,17 +37,21 @@ class ActorManager:
         new_last_name: str
     ) -> None:
         self._connection.execute(
-            f"""UPDATE {self.table_name} """
-            f"""SET first_name = ?, last_name = ? """
-            f"""WHERE id = ?""",
+            f"""
+            UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?
+            """,
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"""DELETE FROM {self.table_name} """
-            f"""WHERE id = ?""",
+            f"""
+            DELETE FROM {self.table_name}
+            WHERE id = ?
+            """,
             (id_to_delete,)
         )
         self._connection.commit()
