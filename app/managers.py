0bf8d82cdf8ee76d597039aa1e0e9f5e4ed587df
Frezworx <frezworx@gmail.com>
2024-08-25 16:39:26 +0300

    Solution - Create initial database and test various methods in main module

---
+++ app/managers.py
@@ -0,0 +1,47 @@
+import os
+import sqlite3
+
+from models import Actor
+
+
+class ActorManager:
+    def __init__(self):
+        os.chdir(r"C:\projects\projects\py-actor-manager")
+        self._connection = sqlite3.connect("cinema_db.sqlite")
+        self.table_name = "actors"
+
+    def create(self, first_name, last_name):
+        self._connection.execute(
+            f'INSERT INTO {self.table_name} (first_name, last_name) VALUES(?, ?)',
+            (first_name, last_name,)
+        )
+        self._connection.commit()
+
+    def all(self):
+        cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+        return [
+            Actor(*row) for row in cursor
+        ]
+
+    def update(
+            self,
+            id_to_update: int,
+            new_first_name: str,
+            new_last_name: str
+    ):
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ? ",
+            (id_to_update, new_first_name, new_last_name)
+        )
+        self._connection.commit()
+
+    def delete(self, id_to_delete: int):
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ? ",
+            (id_to_delete,)
+        )
+        self._connection.commit()
