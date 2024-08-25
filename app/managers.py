5c3eaa98aceac8afeb4524ce40cc9af485c6fe39
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-25 10:34:26 +0300

    implement py-actor-manager(used triple quotes)

--- app/managers.py
+++ app/managers.py
@@ -9,16 +9,19 @@ class CinemaManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) "
-            f"VALUES (?, ?)",
+            f"""
+            INSERT INTO {self.table_name} (first_name, last_name)
+            VALUES (?, ?)
+            """,
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         cinema_cursor = self._connection.execute(
-            f"SELECT * "
-            f"FROM {self.table_name}"
+            f"""
+            SELECT * FROM {self.table_name}
+            """
         )
         actors = [Actor(*row) for row in cinema_cursor]
         cinema_cursor.close()
@@ -26,17 +29,21 @@ class CinemaManager:
 
     def update(self, _id: int, new_name: str, new_last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} "
-            f"SET first_name = ?, last_name = ? "
-            f"WHERE id = ?",
+            f"""
+            UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?
+            """,
             (new_name, new_last_name, _id)
         )
         self._connection.commit()
 
     def delete(self, _id: int) -> None:
         self._connection.execute(
-            f" DELETE FROM {self.table_name}  "
-            f"WHERE id = ?",
+            f"""
+            DELETE FROM {self.table_name}
+            WHERE id = ?
+            """,
             (_id,)
         )
         self._connection.commit()
