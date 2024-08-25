1b729afeef5cced87f776be7e630db73693c527d
Ivan Shakhman <shakhmanivan@gmail.com>
2024-08-25 15:07:31 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -11,7 +11,7 @@ class ActorManager:
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"""
-            INSERT INTO {self.table_name} (first_name, last_name) 
+            INSERT INTO {self.table_name} (first_name, last_name)
             VALUES (?, ?)
                 """,
             (first_name, last_name)
