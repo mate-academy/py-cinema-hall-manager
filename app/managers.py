fbc4b588b259282af8fb81ed56d00c7bf806c336
Frezworx <frezworx@gmail.com>
2024-08-26 11:17:27 +0300

    Fix: added blank line between different import groups

--- app/managers.py
+++ app/managers.py
@@ -5,19 +5,19 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinema_db.sqlite")
+        self._connect = sqlite3.connect("cinema_db.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self._connection.execute(
+        self._connect.execute(
             f"INSERT INTO {self.table_name} "
             f"(first_name, last_name) VALUES(?, ?)",
             (first_name, last_name,)
         )
-        self._connection.commit()
+        self._connect.commit()
 
     def all(self) -> list:
-        cursor = self._connection.execute(
+        cursor = self._connect.execute(
             f"SELECT * FROM {self.table_name}"
         )
         return [
@@ -30,17 +30,17 @@ class ActorManager:
             new_first_name: str,
             new_last_name: str
     ) -> None:
-        self._connection.execute(
+        self._connect.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ? "
             "WHERE id = ? ",
             (id_to_update, new_first_name, new_last_name)
         )
-        self._connection.commit()
+        self._connect.commit()
 
     def delete(self, id_to_delete: int) -> None:
-        self._connection.execute(
+        self._connect.execute(
             f"DELETE FROM {self.table_name} WHERE id = ? ",
             (id_to_delete,)
         )
-        self._connection.commit()
+        self._connect.commit()
