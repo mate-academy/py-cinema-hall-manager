49841ee877605f613f6cba47784cc7f5d3642f5b
user <graban2253@gmail.com>
2024-09-03 23:09:46 +0300

    Finished

--- app/managers.py
+++ app/managers.py
@@ -34,7 +34,8 @@ class ActorManager:
         )
         self.connection.commit()
 
-    def delete(self, id_to_delete: int) -> None:
+    def delete(self,
+               id_to_delete: int) -> None:
         self.connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?",
             (id_to_delete,)
