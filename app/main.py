6db46671e06cb4f7454a87c4f5d131cdad7a2425
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-09-03 10:52:54 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-# from models import Actor
-# from managers import ActorManager
-#
-# if __name__ == "__main__":
-#     Actor.objects = ActorManager()
-#
-#     Actor.objects.create(first_name="Emma", last_name="Watson")
-#     Actor.objects.create(first_name="Daniel", last_name="Radclife")
-#     print(Actor.objects.all())
-#     Actor.objects.update(2, "Daniel", "Radcliffe")
-#     print(Actor.objects.all())
-#     Actor.objects.delete(1)
-#     print(Actor.objects.all())
+from models import Actor
+from manager import ActorManager
+
+if __name__ == "__main__":
+    Actor.objects = ActorManager()
+
+    Actor.objects.create(first_name="Walter", last_name="White")
+    Actor.objects.create(first_name="Jessy", last_name="Pinkman")
+    print(Actor.objects.all())
+    Actor.objects.update(2, "Gustavo", "Fring")
+    print(Actor.objects.all())
+    Actor.objects.delete(1)
+    print(Actor.objects.all())
