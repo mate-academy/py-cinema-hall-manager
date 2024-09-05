b3c1930c3669ee4d4467f57bc5184c663cbaa7b5
Alex Bul <tarasov57228@gmail.com>
2024-09-05 16:26:01 +0300

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -7,7 +7,7 @@ class ActorManager:
 
     def __init__(self) -> None:
         self._connection = sqlite3.connect(
-            "cinema.sqlite"
+            "../cinema.sqlite"
         )
         self.table_name = "actors"
 
