c14014764b6519a2176ff62430a559866a179a08
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-09-08 02:00:47 -0700

    Realized database manager by Python

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 # from models import Actor
 # from managers import ActorManager
-#
+
 # if __name__ == "__main__":
 #     Actor.objects = ActorManager()
-#
+
 #     Actor.objects.create(first_name="Emma", last_name="Watson")
 #     Actor.objects.create(first_name="Daniel", last_name="Radclife")
 #     print(Actor.objects.all())
