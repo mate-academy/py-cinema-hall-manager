ca38415ed802f890cbd180f960940636029c1669
Max <mxbely98@gmail.com>
2024-10-19 14:46:58 +0300

    created CRUD to Actors table

--- app/managers.py
+++ app/managers.py
@@ -6,25 +6,27 @@ from models import Actor
 
 @dataclass
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("../cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name, last_name) -> None:
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"insert into {self.table_name} (first_name, last_name) values (?, ?)",
+            f"insert into {self.table_name} "
+            f"(first_name, last_name) values (?, ?)",
             (first_name, last_name,)
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
             f"select * from {self.table_name}"
         )
         self._connection.commit()
         return [Actor(*row) for row in actors_cursor]
 
-    def update(self, id_to_update, first_name, last_name) -> None:
+    def update(self, id_to_update: int,
+               first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"update {self.table_name} "
             "set first_name = ?, last_name = ? "
@@ -33,7 +35,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete) -> None:
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
             f"delete from {self.table_name} where id = ?",
             (id_to_delete,)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Actor:
     id: int
     first_name: str
     last_name: str
-
