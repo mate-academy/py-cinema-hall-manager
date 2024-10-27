3f035b65e2a060c465b26f70e9d361c8bdae55c3
Danulo <danulo1337@gmail.com>
2024-10-27 20:18:51 +0200

    Fixed Solution

--- app/managers.py
+++ app/managers.py
@@ -5,7 +5,7 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.Connection("cinema.sqlite")
+        self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
@@ -31,9 +31,11 @@ class ActorManager:
             "WHERE id=?",
             (first_name, last_name, actor_id)
         )
+        self._connection.commit()
 
     def delete(self, actor_id: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id=?",
             (actor_id,)
         )
+        self._connection.commit()
