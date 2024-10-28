ca448644a2f0c5c7aaf9bfc4c870d6014a5f067a
user <nikitaskrypets@gmail.com>
2024-10-28 22:52:53 +0200

    solved flake8

--- app/managers.py
+++ app/managers.py
@@ -2,18 +2,19 @@ import sqlite3
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self._table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
-        cursor = self._connection.execute(
-            f"INSERT INTO {self._table_name} (first_name, last_name) VALUES (?, ?)",
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self._table_name}"
+            f" (first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         cursor = self._connection.execute(
             f"SELECT * FROM {self._table_name}"
         )
@@ -21,16 +22,16 @@ class ActorManager:
             row for row in cursor
         ]
 
-    def delete(self, id_: int):
-        cursor = self._connection.execute(
+    def delete(self, id_: int) -> None:
+        self._connection.execute(
             f"DELETE FROM {self._table_name} WHERE id = ?",
             (id_,)
         )
 
         self._connection.commit()
 
-    def update(self, id_: int, first_name: str, last_name: str):
-        cursor = self._connection.execute(
+    def update(self, id_: int, first_name: str, last_name: str) -> None:
+        self._connection.execute(
             f"UPDATE {self._table_name} "
             "SET first_name = ?, last_name = ? "
             "WHERE id = ?",
