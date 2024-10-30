53e5cb79dcd2a74bbd30e9b79edfaa2e94c462c4
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-30 16:35:11 +0200

    Fixed flake8

--- app/managers.py
+++ app/managers.py
@@ -6,12 +6,13 @@ from app.models import Actor
 class ActorManager:
 
     def __init__(self) -> None:
-        self._connection = sqlite3.connect('../cinema.sqlite')
-        self.table_name = 'actors'
+        self._connection = sqlite3.connect("../cinema.sqlite")
+        self.table_name = "actors"
 
-    def create(self, first_name: str, last_name) -> None:
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
@@ -22,9 +23,13 @@ class ActorManager:
         )
         return [Actor(*row) for row in actors_cursor]
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name) -> None:
+    def update(self,
+               id_to_update: int,
+               new_first_name: str,
+               new_last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name} "
+            f"SET first_name=?, last_name=? WHERE id=?",
             (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
