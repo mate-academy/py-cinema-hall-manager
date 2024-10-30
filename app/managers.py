c82e77e5335152865f0e4d665e18a07ef5855b0d
Mike Shmygel <m.shmygel@gmail.com>
2024-10-30 22:01:25 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -9,7 +9,9 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)", (first_name, last_name)
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
         )
         self._connection.commit()
 
@@ -20,7 +22,11 @@ class ActorManager:
 
         return [Actor(*row) for row in actor_cursor]
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name: str) -> None:
+    def update(self,
+               id_to_update: int,
+               new_first_name: str,
+               new_last_name: str
+               ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ? "
@@ -28,7 +34,6 @@ class ActorManager:
             (id_to_update, new_first_name, new_last_name)
         )
         self._connection.commit()
-
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
