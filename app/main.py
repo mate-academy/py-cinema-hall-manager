438ff8af712eb626d9c4460dd8d738fd2d051f73
Mike Shmygel <m.shmygel@gmail.com>
2024-10-31 07:55:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     Actor.objects = ActorManager()
 
     Actor.objects.create(first_name="Emma", last_name="Watson")
-    Actor.objects.create(first_name="Daniel", last_name="Radclife")
+    Actor.objects.create(first_name="Daniel", last_name="Radcliffe")
     print(Actor.objects.all())
     Actor.objects.update(2, "Daniel", "Radcliffe")
     print(Actor.objects.all())
--- app/managers.py
+++ app/managers.py
@@ -31,7 +31,7 @@ class ActorManager:
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ? "
             "WHERE id = ? ",
-            (id_to_update, new_first_name, new_last_name)
+            (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
