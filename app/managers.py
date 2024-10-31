ec02dd4381dcd6704db0af03e0b1aca46ac4f668
Artem Shatylo <Alukard32244@gmail.com>
2024-10-31 20:28:42 +0200

    I hope it last Solution

--- app/managers.py
+++ app/managers.py
@@ -5,17 +5,16 @@ from app.models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self.table_name = 'actors'
-        self.connection = sqlite3.connect('cinema.sqlite')
-
+        self.table_name = "actors"
+        self.connection = sqlite3.connect("cinema.sqlite")
 
     def create(self, first_name: str, last_name: str) -> None:
         self.connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self.connection.commit()
-
 
     def all(self) -> list:
         cinema_cursor = self.connection.execute(
@@ -27,7 +26,8 @@ class ActorManager:
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
         self.connection.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name} "
+            "SET first_name=?, last_name=? WHERE id=?",
             (first_name, last_name, id)
         )
         self.connection.commit()
@@ -37,8 +37,3 @@ class ActorManager:
             f"DELETE FROM {self.table_name} WHERE id=?",
             (id,)
         )
-
-
-if __name__ == '__main__':
-    manager = ActorManager()
-    manager.all()
