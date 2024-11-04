8763b331539872fbc400d0f6816aadc77bfa54bf
Volodymyr Kovalenko <380974261314kv@gmail.com>
2024-11-04 18:27:56 +0200

    fixed docstring quotes

--- app/managers.py
+++ app/managers.py
@@ -5,14 +5,14 @@ from models import Actor
 
 
 class ActorManager:
-    'Create ActorManager class'
+    """Create ActorManager class"""
 
     def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        'Create a new actor'
+        """Create a new actor"""
         self._connection.execute(
             f"INSERT INTO {self.table_name} "
             "(first_name, last_name) VALUES (?, ?)",
@@ -21,14 +21,14 @@ class ActorManager:
         self._connection.commit()
 
     def all(self) -> List[Actor]:
-        'Getting all records from the table'
+        """Getting all records from the table"""
         cursor = self._connection.execute(f"SELECT * FROM {self.table_name}")
         return [Actor(*row) for row in cursor]
 
     def update(
         self, id_to_update: int, new_name: str, new_last_name: str
     ) -> None:
-        'Update record by id'
+        """Update record by id"""
         self._connection.execute(
             f"UPDATE {self.table_name} "
             "SET first_name = ?, last_name = ? WHERE id = ?",
@@ -37,7 +37,7 @@ class ActorManager:
         self._connection.commit()
 
     def delete(self, id_to_delete: int) -> None:
-        'Delete record by id'
+        """Delete record by id"""
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?", (id_to_delete,)
         )
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Actor:
-    'Class Actor with attributes id, first_name and last_name'
+    """Class Actor with attributes id, first_name and last_name"""
 
     id: int
     first_name: str
