a20e8384959234c16345c5a8b3c7c8e28d6f4bd3
Max Savenya <savenya2013@gmail.com>
2024-11-04 09:11:59 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -38,7 +38,7 @@ class ActorManager:
 
     def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE {self.table_name} "
+            f"DELETE FROM {self.table_name} "
             "WHERE id = ?",
             (id_to_delete,)
         )
