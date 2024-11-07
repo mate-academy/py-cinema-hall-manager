1601056420a088142170eca9b98ae8ec82e74ce0
srx <srx92@protonmail.com>
2024-11-07 17:40:12 +0200

    Solution

---
+++ app/managers.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+from app.models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self.table_name = "actors"
+        self._connection = sqlite3.connect("cinema.sqlite")
+
+    def create(
+            self,
+            first_name: str,
+            last_name: str,
+    ) -> None:
+        self._connection.execute(
+            f"INSERT INTO  {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?) ",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> list:
+        actor_connection_cursor = self._connection.execute(
+            f"SELECT * FROM  {self.table_name}"
+        )
+        return [
+            Actor(*actor)
+            for actor in actor_connection_cursor
+        ]
+
+    def update(
+            self,
+            update_id: int,
+            first_name_update: str,
+            last_name_update: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = ?",
+            (
+                first_name_update,
+                last_name_update,
+                update_id
+            )
+        )
+        self._connection.commit()
+
+    def delete(
+            self,
+            delete_id: int
+    ) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            f"WHERE id = ?",
+            (delete_id,)
+        )
+        self._connection.commit()
