7e2a382980a36807a1e37e3a87128644b10f4458
Rudge0 <maksim.pariychuk07@gmail.com>
2024-11-13 19:56:52 +0100

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,21 +4,21 @@ import sqlite3
 
 class ActorManager:
     def __init__(self) -> None:
-        self.connect_ = sqlite3.connect("cinema.sqlite")
-        self.tabel_name = "actors"
+        self._connect = sqlite3.connect("cinema.sqlite")
+        self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
-        self.connect_.execute(
-            f"INSERT INTO {self.tabel_name} "
+        self._connect.execute(
+            f"INSERT INTO {self.table_name} "
             f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name,)
         )
 
-        self.connect_.commit()
+        self._connect.commit()
 
     def all(self) -> list:
-        info = self.connect_.execute(
-            f"SELECT * FROM {self.tabel_name}"
+        info = self._connect.execute(
+            f"SELECT * FROM {self.table_name}"
         )
 
         return [
@@ -26,24 +26,18 @@ class ActorManager:
         ]
 
     def update(self, id_: int, first_name: str, last_name: str) -> None:
-        self.connect_.execute(
-            f"UPDATE {self.tabel_name} "
+        self._connect.execute(
+            f"UPDATE {self.table_name} "
             f"SET first_name = ?, last_name = ? WHERE id = ?",
             (first_name, last_name, id_)
         )
 
-        self.connect_.commit()
+        self._connect.commit()
 
     def delete(self, id_: int) -> None:
-        self.connect_.execute(
-            f"DELETE FROM {self.tabel_name} WHERE id = ?",
+        self._connect.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
             (id_,)
         )
 
-        self.connect_.commit()
-
-
-database = ActorManager()
-
-database.create("maks", "p")
-print(database.all())
+        self._connect.commit()
