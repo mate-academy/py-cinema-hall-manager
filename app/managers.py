3147df7ccdf7376724c868dec3b42019a0234ba0
Oleksandr Merlenko <s.merlenko@gmail.com>
2024-11-25 14:52:37 +0100

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,18 +4,19 @@ from models import Actor
 
 class ActorManager:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("../cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list:
         actors_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -23,7 +24,7 @@ class ActorManager:
             Actor(*row) for row in actors_cursor
         ]
 
-    def update(self, id: int, first_name: str, last_name: str):
+    def update(self, id: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
             "WHERE id = ?",
@@ -31,7 +32,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-    def delete(self, id: int):
+    def delete(self, id: int) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id = ?",
             (id,)
