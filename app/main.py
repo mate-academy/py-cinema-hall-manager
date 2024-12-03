b9293886ec729b3943010a425fdfe6726027152a
Tymofii Pavliohlo <tim13fiz@gmail.com>
2024-12-03 15:49:29 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ from managers import ActorManager
 if __name__ == "__main__":
     Actor.objects = ActorManager()
 
-    Actor.objects.create(first_name="Tom", last_name="Henks")
-    Actor.objects.create(first_name="Tina", last_name="Brauni")
+    Actor.objects.create(first_name="Emma", last_name="Watson")
+    Actor.objects.create(first_name="Daniel", last_name="Radclife")
     print(Actor.objects.all())
     Actor.objects.delete(1)
     print(Actor.objects.all())
