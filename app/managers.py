11c996c8e7d86eae9caf1bc4a96397ac1a52b71e
Tymofii Pavliohlo <tim13fiz@gmail.com>
2024-12-05 21:00:24 +0100

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,47 +4,35 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self) -> None:
-        self._connection = sqlite3.connect("cinemadb.sqlite3")
-        self.table_name = "actors"
-        self._create_actors_table()
 
-    def _create_actors_table(self) -> None:
-        self._connection.execute(
-            f"CREATE TABLE IF NOT EXISTS {self.table_name} "
-            "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-            "first_name TEXT, last_name TEXT)"
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema.sqlite")
+        self._table_name = "actors"
+
+    def all(self) -> list:
+        cinema_actor_cursor = self._connection.execute(
+            f"SELECT * FROM {self._table_name}"
         )
-        self._connection.commit()
+        return [Actor(*obj) for obj in cinema_actor_cursor.fetchall()]
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} "
+            f"INSERT INTO {self._table_name} "
             f"(first_name, last_name) VALUES (?, ?)",
-            (first_name, last_name)
+            (first_name, last_name,)
         )
         self._connection.commit()
 
-    def all(self) -> list:
-        actors_cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
-        )
-        return [
-            Actor(*row) for row in actors_cursor
-        ]
-
-    def update(self, id_to_update: int,
-               first_name: str, last_name: str) -> None:
+    def update(self, obj_id: int, first_name: str, last_name: str, ) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ? WHERE id = ?",
-            (first_name, last_name, id_to_update)
+            f"UPDATE {self._table_name} "
+            f"SET first_name = ?, last_name = ? WHERE id = ?",
+            (first_name, last_name, obj_id)
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete: int) -> None:
+    def delete(self, obj_id: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ? ",
-            (id_to_delete,)
+            f"DELETE FROM {self._table_name}  WHERE id = ?", (obj_id,)
         )
         self._connection.commit()
