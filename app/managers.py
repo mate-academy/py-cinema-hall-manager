b8e99b8aff37fa67800501c29f07018bfd78157d
Fibonacci <volokitin.vitaliy@icloud.com>
2024-12-06 14:33:01 +0200

    Complated

--- app/managers.py
+++ app/managers.py
@@ -4,38 +4,44 @@ from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("checklist.db")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
-            (first_name, last_name)
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) "
+            f"VALUES (?, ?)",
+            (first_name, last_name),
         )
         self._connection.commit()
 
-    def all(self):
-        actors_format_cursor = self._connection.execute(
+    def all(self) -> list[Actor]:
+        cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
-        return [
-            Actor(*row) for row in actors_format_cursor
-        ]
+        return [Actor(*row) for row in cursor]
 
-    def update(self, id_to_update: int, new_first_name: str, new_last_name: str):
+    def update(
+            self,
+            id_to_update: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ? "
-            "WHERE id = ? ",
-            (new_first_name, new_last_name, id_to_update)
-
+            f"""
+            UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?
+            """,
+            (new_first_name, new_last_name, id_to_update),
         )
         self._connection.commit()
 
-    def delete(self, id_to_delete):
+    def delete(self, id_to_delete: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ? ",
-            (id_to_delete,)
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (id_to_delete,),
         )
         self._connection.commit()
