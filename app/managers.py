141d1d1d7bc67efc0e937f26fac29730f2452071
Vlad Tymoshko <slradbez@gmail.com>
2024-12-12 22:24:25 +0100

    solution

--- app/managers.py
+++ app/managers.py
@@ -7,7 +7,7 @@ from app.models import Actor
 class ActorManager:
     def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
-        self.table_name = 'actors'
+        self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
