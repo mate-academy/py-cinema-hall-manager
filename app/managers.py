e9ebdad99bc93abe9c4e46a14c781b24c220689f
Ruslan Toister <rtoister.cv@gmail.com>
2024-12-13 10:29:38 +0200

    Solution2

--- app/managers.py
+++ app/managers.py
@@ -1,14 +1,15 @@
 import sqlite3
+from typing import List
 
 from models import Actor
 
 
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         query = f"""
         INSERT INTO {self.table_name} (first_name, last_name)
         VALUES (?, ?);
@@ -16,7 +17,7 @@ class ActorManager:
         self._connection.execute(query, (first_name, last_name))
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> List[Actor]:
         literary_format_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -24,7 +25,7 @@ class ActorManager:
             Actor(*row) for row in literary_format_cursor
         ]
 
-    def update(self, id_to_update: int, first_name: str, last_name: str):
+    def update(self, id_to_update: int, first_name: str, last_name: str) -> None:
         query = f"""
         UPDATE {self.table_name}
         SET first_name = ?, last_name = ?
@@ -33,7 +34,7 @@ class ActorManager:
         self._connection.execute(query, (first_name, last_name, id_to_update))
         self._connection.commit()
 
-    def delete(self, id_to_delete: int):
+    def delete(self, id_to_delete: int) -> None:
         query = f"""
         DELETE FROM {self.table_name}
         WHERE id = ?;
