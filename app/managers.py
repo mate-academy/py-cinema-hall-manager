6c71df6935db90ca76fedc5b726f2aeccd25ff76
bashyrov <bashyroov@gmail.com>
2025-01-01 20:55:18 +0100

    Solution

--- app/managers.py
+++ app/managers.py
@@ -2,12 +2,13 @@ import sqlite3
 
 from models import Actor
 
+
 class ActorManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("cinema")
         self.table_name = "actors"
 
-    def all(self):
+    def all(self) -> list:
         actor_cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -16,24 +17,26 @@ class ActorManager:
             Actor(*row) for row in actor_cursor
         ]
 
-    def create(self, first_name, last_name):
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._connection.commit()
 
-    def update(self, id, first_name, last_name):
+    def update(self, id: str, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"Update {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"Update {self.table_name} "
+            f"SET first_name=?, last_name=? WHERE id=?",
             (first_name, last_name, id)
         )
         self._connection.commit()
 
-    def delete(self, id):
+    def delete(self, id: str) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} WHERE id=?",
-     (id,)
+            (id,)
         )
         self._connection.commit()
 
