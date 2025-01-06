3230ec152586a592dfdcbffea979e266e5c8e43c
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-06 16:02:11 +0200

    actors-solution

--- app/managers.py
+++ app/managers.py
@@ -14,24 +14,21 @@ class ActorManager:
                 f"SELECT * FROM {self._TABLE_NAME}")
             return [Actor(*row) for row in actors_cursor]
 
-    def create(self, _first_name : str, _last_name: str ) -> None:
+    def create(self, _first_name : str, _last_name: str) -> None:
         with sqlite3.connect(self._DB_PATH) as conn:
             conn.execute(
                 f"INSERT INTO {self._TABLE_NAME} VALUES (?, ?)",
-                (_first_name, _last_name)
-)
+                (_first_name, _last_name))
 
-    def update(self, _id : int, _first_name : str, _last_name: str ) -> None:
+    def update(self, _id : int, _first_name : str, _last_name: str) -> None:
         with sqlite3.connect(self._DB_PATH) as conn:
             conn.execute(
                 f"UPDATE {self._TABLE_NAME} "
                 f"SET first_name = ?, last_name = ? WHERE id = ?",
-                (_first_name, _last_name, _id)
-)
+                (_first_name, _last_name, _id))
 
     def delete(self, _id : int) -> None:
         with sqlite3.connect(self._DB_PATH) as conn:
             conn.execute(
                 f"DELETE FROM {self._TABLE_NAME} WHERE id = ?",
-                (_id,)
-)
+                (_id,))
