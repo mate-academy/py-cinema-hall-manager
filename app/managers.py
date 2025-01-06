f6f1d91a4532bc43a15a57ab7ae6f57a9afc5490
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-06 15:56:02 +0200

    actors-solution

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+
 from app.models import Actor
+
 
 class ActorManager:
     _DB_PATH = "cinema.sqlite"
@@ -9,18 +11,27 @@ class ActorManager:
     def all(self) -> list[Actor]:
         with sqlite3.connect(self._DB_PATH) as conn:
             actors_cursor = conn.execute(
-                f"SELECT * FROM {self._TABLE_NAME}")
+                f"SELECT * FROM {self._TABLE_NAME}"
+            )
             return [Actor(*row) for row in actors_cursor]
+
+
     def create(self, _first_name : str, _last_name: str ) -> None:
         with sqlite3.connect(self._DB_PATH) as conn:
             conn.execute(
                 f"INSERT INTO {self._TABLE_NAME} VALUES (?, ?)",
-                (_first_name, _last_name))
+                (_first_name, _last_name)
+            )
+
+
     def update(self, _id : int, _first_name : str, _last_name: str ) -> None:
         with sqlite3.connect(self._DB_PATH) as conn:
             conn.execute(
-                f"UPDATE {self._TABLE_NAME} SET first_name = ?, last_name = ? WHERE id = ?",
+                f"UPDATE {self._TABLE_NAME} "
+                f"SET first_name = ?, last_name = ? WHERE id = ?",
                 (_first_name, _last_name, _id))
+
+
     def delete(self, _id : int) -> None:
         with sqlite3.connect(self._DB_PATH) as conn:
             conn.execute(
