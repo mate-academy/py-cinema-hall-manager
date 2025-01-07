2e7491166034da855c99db21eef9a9c071a55878
Vova <vova.gld05@gmail.com>
2025-01-07 17:47:31 +0200

    'fix2'

--- app/managers.py
+++ app/managers.py
@@ -1,7 +1,6 @@
 import sqlite3
 from typing import List
 
-
 from app.models import Actor
 
 
@@ -20,22 +19,26 @@ class ActorManager:
 
     def all(self) -> List[Actor]:
         cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+            f"SELECT * "
+            f"FROM {self.table_name}"
         )
 
         return [Actor(*row) for row in cursor]
 
-    def update(self, id_: int, first_name: str, last_name: str) -> None:
+    def update(self, actor_id: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
-            "SET first_name = ?, last_name = ? WHERE id = ?",
-            (first_name, last_name, id_)
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
+            (first_name, last_name, actor_id)
         )
         self._connection.commit()
 
-    def delete(self, id_: int) -> None:
+    def delete(self, actor_id: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ?",
-            (id_,)
+            f"DELETE "
+            f"FROM {self.table_name} "
+            f"WHERE id = ?",
+            (actor_id,)
         )
         self._connection.commit()
