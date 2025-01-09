e13e3ea27fbe1e1f084a4b734860275bf4d6ee38
Ron Phillips <mcspidy@gmail.com>
2025-01-08 18:02:27 -0600

    Flake8 ' to "

--- app/managers.py
+++ app/managers.py
@@ -7,35 +7,35 @@ class ActorManager:
         # This method is used to establish a connection to the database.
         # It also creates a table to store actors if it does not exist.
         # The table has three columns: id, first_name, and last_name.
-        self.connection = sqlite3.connect('cinema.db')
+        self.connection = sqlite3.connect("cinema.db")
         self.cursor = self.connection.cursor()
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS actors
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS actors
                             (id INTEGER PRIMARY KEY AUTOINCREMENT,
                             first_name TEXT,
-                            last_name TEXT)''')
+                            last_name TEXT)""")
         self.connection.commit()
 
     def create(self, first_name: str, last_name: str) -> None:
         # This method is used to create a new actor in the database.
-        self.cursor.execute('''INSERT INTO actors (first_name, last_name)
-                            VALUES (?, ?)''', (first_name, last_name))
+        self.cursor.execute("""INSERT INTO actors (first_name, last_name)
+                            VALUES (?, ?)""", (first_name, last_name))
         self.connection.commit()
 
     def all(self) -> list[Actor]:
         # This method is used to retrieve all the actors from the database.
-        self.cursor.execute('''SELECT * FROM actors''')
+        self.cursor.execute("""SELECT * FROM actors""")
         actors = self.cursor.fetchall()
         return [Actor(*actor) for actor in actors]
 
     def update(self, id: int, first_name: str, last_name: str) -> None:
         # This method is used to update an actor in the database.
-        self.cursor.execute('''UPDATE actors SET first_name = ?, last_name = ?
-                            WHERE id = ?''', (first_name, last_name, id))
+        self.cursor.execute("""UPDATE actors SET first_name = ?, last_name = ?
+                            WHERE id = ?""", (first_name, last_name, id))
         self.connection.commit()
 
     def delete(self, id: int) -> None:
         # This method is used to delete an actor from the database.
-        self.cursor.execute('''DELETE FROM actors WHERE id = ?''', (id,))
+        self.cursor.execute("""DELETE FROM actors WHERE id = ?""", (id,))
         self.connection.commit()
 
     def __del__(self) -> None:
