281163967a4e405cd7e96bd98f3cdafb97aadf7c
Aleksandr <sasha.syrota15@gmail.com>
2025-01-10 04:34:00 +0200

    made a tests

--- app/managers.py
+++ app/managers.py
@@ -5,7 +5,7 @@ from app.models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect("../cinema.sqlite")
+        self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str = None) -> None:
@@ -24,13 +24,13 @@ class ActorManager:
             self,
             id_to_update: int,
             new_first_name: str,
-            new_lat_name: str
+            new_last_name: str
     ) -> None:
         self._connection.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name=?, last_name=? "
             f"WHERE id=?",
-            (new_first_name, new_lat_name, id_to_update)
+            (new_first_name, new_last_name, id_to_update)
         )
         self._connection.commit()
 
