0299a2eab664a8bd71707a13fa7da51e8d170ea1
Felix Poslushnyi <felixp4@gmail.com>
2025-01-19 20:34:18 +0200

    'Solution'

---
+++ app/managers.py
@@ -0,0 +1,53 @@
+import sqlite3
+from models import Actor
+
+
+class ActorManager:
+    def __init__(self) -> None:
+        self._connection = sqlite3.connect("cinema.db")
+        self.table_name = "actors"
+
+    # CREATE - C
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    # UPDATE - U
+    def update(
+        self,
+        id_to_update: int,
+        new_first_name: str,
+        new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ? ",
+            (new_first_name, new_last_name, id_to_update)
+        )
+        self._connection.commit()
+
+    # DELETE - D
+    def delete(self, id_to_delete: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} "
+            "WHERE id = ? ",
+            (id_to_delete, )
+        )
+        self._connection.commit()
+
+    # RETRIEVE - R
+    def all(self) -> None:
+        actor_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+        # print(f"literary_format_cursor:  {literary_format_cursor}")
+        # for row in literary_format_cursor:
+        #    print(row)
+        return [
+            Actor(*row) for row in actor_cursor
+        ]
